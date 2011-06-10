c0241771578b8fc9cb7551eca908f6d4f86efc67
Anthony Castronova <Castronova.Anthony@gmail.com>
2011-06-10 10:29:03 -0400

    HydroModeler: Re-added the HM binaries.  Reogranized the OpenMI libraries.

---
+++ Source/Main/HydroDesktop.Main/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/****************
+ * THIS WILL BE AUTOMATICALLY REPLACED IN THE AUTOMATED BUILD 
+ * PROCESS
+ * ************************************************/
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HydroDesktop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CUAHSI")]
+[assembly: AssemblyProduct("HydroDesktop")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c7aa218-d015-40ed-b09e-ed92a9daf26c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("0.1.0.31288")]
+//[assembly: AssemblyFileVersion("0.1.0.31288")]
+/****************
+ * THIS WILL BE AUTOMATICALLY REPLACED IN THE AUTOMATED BUILD 
+ * PROCESS
+ * ************************************************/
+[assembly: AssemblyVersion("1.2.537.0")]
+ 
