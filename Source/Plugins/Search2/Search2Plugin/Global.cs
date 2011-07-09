046f272d770c599ad1960a7c02557573227c93ee
mogikanin tir <mogikanin.tir@gmail.com>
2011-07-09 17:11:47 +0400

    Search layer with custom symbology (size range)

---
+++ Source/Plugins/Search2/Search2Plugin/Global.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HydroDesktop.Configuration;
+
+namespace HydroDesktop.Search
+{
+    static class Global
+    {
+        public static string GetHISCentralURL()
+        {
+            return Settings.Instance.SelectedHISCentralURL;
+        }
+
+        internal static readonly string SEARCH_RESULT_LAYER_NAME = Resources.SEARCH_RESULT_LAYER_NAME;
+    }
+}
