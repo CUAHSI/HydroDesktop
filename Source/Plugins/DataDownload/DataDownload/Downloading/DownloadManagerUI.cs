43a21223d595f2b00748a11ea4d74304e4f0361d
mogikanin tir <mogikanin.tir@gmail.com>
2011-07-31 20:45:29 +0400

    Removed redundant code

--- Source/Plugins/DataDownload/DataDownload/Downloading/DownloadManagerUI.cs
+++ Source/Plugins/DataDownload/DataDownload/Downloading/DownloadManagerUI.cs
@@ -6,9 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using HydroDesktop.Configuration;
-using HydroDesktop.Database;
-using HydroDesktop.Interfaces;
 
 namespace HydroDesktop.DataDownload.Downloading
 {
