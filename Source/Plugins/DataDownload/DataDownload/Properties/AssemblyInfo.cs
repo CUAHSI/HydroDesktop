d48715f2fa8add76eee237e542538be6afeb5204
jirikadlec2 <jirikadlec2@localhost>
2011-08-09 19:32:52 -0600

    include DataDownload plugin in the installer

--- Source/Plugins/DataDownload/DataDownload/Properties/AssemblyInfo.cs
+++ Source/Plugins/DataDownload/DataDownload/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DataDownload")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CUAHSI")]
 [assembly: AssemblyProduct("DataDownload")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
