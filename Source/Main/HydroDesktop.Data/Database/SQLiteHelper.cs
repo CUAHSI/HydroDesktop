4031764652a1e9bc1c250e08779bb4fd4f77f091
mogikanin <mogikanin.tir@gmail.com>
2011-08-26 13:05:14 +0400

    TableView: Change Database - added database schema checking (Item #8232)

--- Source/Main/HydroDesktop.Data/Database/SQLiteHelper.cs
+++ Source/Main/HydroDesktop.Data/Database/SQLiteHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Reflection;
 using System.Diagnostics;
@@ -161,6 +163,127 @@ namespace HydroDesktop.Database
                 }
             }
             return true;
+        }
+
+        /// <summary>
+        /// Validate database schema.
+        /// If schema not valid - InvalidDatabaseSchemaException throws.
+        /// </summary>
+        /// <param name="databaseToCheck">Path to SQLite checked database.</param>
+        /// <param name="databaseType">Database type.</param>
+        /// <exception cref="InvalidDatabaseSchemaException">Throws if database schema not valid.</exception>
+        public static void CheckDatabaseSchema(string databaseToCheck, DatabaseType databaseType)
+        {
+            if (!DatabaseExists(databaseToCheck))
+                throw new InvalidDatabaseSchemaException("Database not exists");
+
+            var originalDbPath = Path.GetTempFileName();
+            try
+            {
+                bool dbCreated;
+                switch (databaseType)
+                {
+                    case DatabaseType.DefaulDatabase:
+                        dbCreated = CreateSQLiteDatabase(originalDbPath);
+                        break;
+                    case DatabaseType.MetadataCacheDatabase:
+                        dbCreated = CreateMetadataCacheDb(originalDbPath);
+                        break;
+                    default:
+                        dbCreated = false;
+                        break;
+                }
+                if (!dbCreated)
+                    throw new InvalidDatabaseSchemaException("Unable to create original database");
+
+                var originalDbOperations = new DbOperations(GetSQLiteConnectionString(originalDbPath),
+                                                    Interfaces.DatabaseTypes.SQLite);
+
+                var checkedlDbOperations = new DbOperations(GetSQLiteConnectionString(databaseToCheck),
+                                                    Interfaces.DatabaseTypes.SQLite);
+
+                const string allTablesQuery = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name";
+
+                var originalTables = originalDbOperations.LoadTable(allTablesQuery);
+                var checkedTables = checkedlDbOperations.LoadTable(allTablesQuery);
+
+                var sbErrors = new StringBuilder();
+                foreach(DataRow row in originalTables.Rows)
+                {
+                    var tableName = row["name"].ToString();
+
+                    // Check for table existing
+                    var rows = checkedTables.Select(string.Format("name = '{0}'", tableName));
+                    if (rows.Length == 0)
+                    {
+                        sbErrors.AppendLine(string.Format("Table '{0}' not found", tableName));
+                        continue;
+                    }
+
+                    // Check for table schema
+                    var schemaQuery = string.Format("PRAGMA table_info({0})", tableName);
+                    var originalSchema = originalDbOperations.LoadTable(schemaQuery);
+                    var checkedSchema = checkedlDbOperations.LoadTable(schemaQuery);
+
+                    foreach(DataRow schemaRow in originalSchema.Rows)
+                    {
+                        var columnName = schemaRow["name"].ToString();
+
+                        var columnRows = checkedSchema.Select(string.Format("name = '{0}'", columnName));
+                        if (columnRows.Length == 0)
+                        {
+                            sbErrors.AppendLine(string.Format("Table '{0}': column '{1}' not found", tableName, columnName));
+                            continue;
+                        }
+                    }
+                }
+
+                if (sbErrors.Length != 0)
+                {
+                    sbErrors.Remove(sbErrors.Length - Environment.NewLine.Length, Environment.NewLine.Length); // remove last new line delimeter
+                    throw new InvalidDatabaseSchemaException(sbErrors.ToString());
+                }
+
+            }catch(Exception ex)
+            {
+                if (ex is InvalidDatabaseSchemaException) throw;
+                throw new InvalidDatabaseSchemaException("Unable to check database schema", ex);
+            }
+            finally
+            {
+                File.Delete(originalDbPath);
+            }
+        }
+    }
+
+    public enum DatabaseType
+    {
+        DefaulDatabase,
+        MetadataCacheDatabase
+    }
+
+    public class InvalidDatabaseSchemaException : Exception
+    {
+        public InvalidDatabaseSchemaException()
+        {
+        }
+
+        public InvalidDatabaseSchemaException(string message)
+            : this(message, null)
+        {
+
+        }
+
+        public InvalidDatabaseSchemaException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+
+        protected InvalidDatabaseSchemaException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+
         }
     }
 }
