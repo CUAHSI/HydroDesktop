e8fc157cdd906b5417976be478f9dca609a27fa1
Mostafa Elag <elag@email.sc.edu>
2011-08-27 01:26:12 -0400

    HydroModeler: Add the ponding time calculation to Phillip's component

--- Source/Plugins/HydroModeler/Components/PhillipEquation/Source/PhillipEquation.cs
+++ Source/Plugins/HydroModeler/Components/PhillipEquation/Source/PhillipEquation.cs
@@ -26,17 +26,31 @@ namespace PhillipEquation
         double m_z;// deacy factor of conductivity with depth
         double p; //porosity deacy parameter
         double Pa_entry; //air pressure entery.
-        double[] Ksat_surf; //saturated hydraulic conductivity at surface[m/day]
-        double[] P_surf; //porosity at surface
+        double Pa_f;//air entery profile.
+        double Ksat_surf; //saturated hydraulic conductivity at surface[m/day]
+        double K_sat;//saturated hydraulic conductivity of soil profile[m/day]
+        double P_surf; //porosity at surface
+        double porosity; // porosity profile 
+        double theta;
+        double tp; //time to pond
+        double Sp;//Sorptivity
+        double duration;//time step in hrs
         List<double> PPT = new List<double>();
         string[] _output_elementset;
         string[] _output_quantity;
+        bool isSoilSaturated = false;
 
         //output parameters
         Dictionary<DateTime, double> outputValues = new Dictionary<DateTime, double>();
         string[] _input_elementset;
         string[] _input_quantity;
+        double infltration;
+        double runoff;
         ArrayList q_infltration_outputs = new ArrayList();
+        ArrayList Q_runoff_outputs = new ArrayList();
+        ArrayList _DateTimes = new ArrayList();
+        Dictionary<DateTime, double> infltration_outputValues = new Dictionary<DateTime, double>();
+        Dictionary<DateTime, double> runoff_outputValues = new Dictionary<DateTime, double>();
         string outputPath = System.IO.Directory.GetCurrentDirectory() + "/output";
         # endregion
 
@@ -85,6 +99,10 @@ namespace PhillipEquation
             m_z = Convert.ToDouble(properties["m_z"]);
             Pa_entry = Convert.ToDouble(properties["Pa_entry"]);
             Z = Convert.ToDouble(properties["Z"]);
+            S = Convert.ToDouble(properties["S"]);
+           Ksat_surf = Convert.ToDouble(properties["Ksat_surf"]);
+           P_surf = Convert.ToDouble(properties["P_surf"]);
+           p = Convert.ToDouble(properties["p"]);
 
             //set OpenMI internal variables
             this.SetVariablesFromConfigFile(Config);
@@ -131,7 +149,74 @@ namespace PhillipEquation
 
         public override bool PerformTimeStep()
         {
-            throw new NotImplementedException();
+            //Identifying the current time step & the start time and detrimine the current time step
+            TimeStamp time = (TimeStamp)this.GetCurrentTime();
+            DateTime curr_time = CalendarConverter.ModifiedJulian2Gregorian(time.ModifiedJulianDay);
+            _DateTimes.Add(curr_time);
+            TimeStamp times = (TimeStamp)this.GetTimeHorizon().Start;
+            DateTime start_time = CalendarConverter.ModifiedJulian2Gregorian(times.ModifiedJulianDay);
+            System.TimeSpan tspan = curr_time - start_time;
+            duration = tspan.Days ;// Days conter
+            int k = Convert.ToInt32(duration);
+
+
+            //infiltration for unsaturated soil
+            if (S < 1 && Ksat_surf > 0)
+            {
+                if (m_z > 0) { K_sat = m_z * Ksat_surf * (1 - Math.Exp(-Z / m_z) / Z); }
+
+                else { K_sat = Ksat_surf; }
+
+                if (p < 1000) { porosity = p * P_surf * (1 - Math.Exp(-Z / p)) / Z; }
+
+                else { porosity = P_surf; }
+
+                theta = S * porosity;
+
+                //Estimate Sorptivity
+                Pa_f = 0.76 * Pa_entry;
+
+                Sp = Math.Sqrt(2) * K_sat * Pa_f;
+
+                //calculate ponding time using Green & Ampt method
+                if (PPT[k] > K_sat) { tp = K_sat * Pa_f * ((porosity - theta) / (PPT[k] * (PPT[k] - K_sat))); }
+
+                else { tp = duration; }
+
+                //calculate the infiltration 
+                double t = duration - tp;
+                if (duration <= tp) { infltration = PPT[k]; }
+                else {infltration = Sp * Math.Pow(t,  0.5) + K_sat * 1.5 / 3.0 * t + tp * PPT[k];}
+                infltration_outputValues.Add(curr_time,infltration);
+            }
+
+            else {infltration = 0;}
+            if (isSoilSaturated)
+            {
+                double _dt = this.GetTimeStep() / 86400;
+                Z = Z - (infltration /_dt);
+                if (Z <= 0) { isSoilSaturated = true; Z = 0; } //need to add a moisture content function with time
+            }
+
+            //calculate the runoff using water balance
+            runoff = PPT[k] - infltration;
+            runoff_outputValues.Add(curr_time,runoff);
+           
+            //set the basin infiltration and runoff as outputs
+            double[] infiltration_save = new double[1];
+            infiltration_save[0] = infltration;// set the infiltration as an scalar output;
+            string q1 = this.GetOutputExchangeItem(0).Quantity.ID;
+            string e1 = this.GetOutputExchangeItem(0).ElementSet.ID;
+            this.SetValues(q1, e1, new ScalarSet(infiltration_save));
+            
+            double[] runoff_save = new double[1];
+            runoff_save[0] = runoff;
+            string q2 = this.GetOutputExchangeItem(1).Quantity.ID;
+            string e2 = this.GetOutputExchangeItem(1).ElementSet.ID;
+            this.SetValues(q2, e2, new ScalarSet(runoff_save));
+
+            this.AdvanceTime();
+            return true;
         }
         # region to read input ruster 
         ///// <summary>
