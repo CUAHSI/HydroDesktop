d7816ebea3355f0613ff3e9c7bb98ef28e77bc37
jirikadlec2 <jirikadlec2@localhost>
2011-09-02 23:18:06 -0600

    add missing xml documentation tags

--- Source/Main/HydroDesktop.Interfaces/HydroDesktop.Interfaces/ObjectModel/DataFile.cs
+++ Source/Main/HydroDesktop.Interfaces/HydroDesktop.Interfaces/ObjectModel/DataFile.cs
@@ -5,22 +5,45 @@ using System.Text;
 namespace HydroDesktop.Interfaces.ObjectModel
 {
     /// <summary>
-    // represents either a WaterML data file returned
-    // by the WaterOneFlow service or a source file used
-    // for importing data into HydroDesktop.
+    /// represents either a WaterML data file returned
+    /// by the WaterOneFlow service or a source file used
+    /// for importing data into HydroDesktop.
     /// </summary>
     public class DataFile : BaseEntity
     {
-        //public int FileID { get; set; }
+        /// <summary>
+        /// The WaterML file name
+        /// </summary>
         public virtual string FileName { get; set; }
+        /// <summary>
+        /// The WaterML file description
+        /// </summary>
         public virtual string FileDescription { get; set; }
+        /// <summary>
+        /// The WaterML file type
+        /// </summary>
         public virtual string FileType { get; set; }
+        /// <summary>
+        /// The WaterML file path
+        /// </summary>
         public virtual string FilePath { get; set; }
+        /// <summary>
+        /// The WaterML file origin
+        /// </summary>
         public virtual string FileOrigin { get; set; }
+        /// <summary>
+        /// The method used for retrieving the WaterML file
+        /// (from GetValues call, created by HydroDesktop, other method)
+        /// </summary>
         public virtual string LoadMethod { get; set; }
+        /// <summary>
+        /// The time of loading data from the  WaterML file
+        /// </summary>
         public virtual DateTime LoadDateTime { get; set; }
         //public virtual int QueryID { get; set; }
-
+        /// <summary>
+        /// Information about WaterML query parameters
+        /// </summary>
         public virtual QueryInfo QueryInfo { get; set; }
 
         /// <summary>
@@ -31,6 +54,7 @@ namespace HydroDesktop.Interfaces.ObjectModel
             get { return (QueryInfo != null); }
         }
 
+        /// <inheritdoc/>
         public override bool Equals(BaseEntity other)
         {
             DataFile file2 = other as DataFile;
@@ -43,7 +67,7 @@ namespace HydroDesktop.Interfaces.ObjectModel
                 return (FileName.Equals(file2.FileName) && FilePath.Equals(file2.FilePath));
             }
         }
-
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             return (FileName + FilePath).GetHashCode();
