2453d50c39c922f35f7bd3de79d527b64333cb40
jirikadlec2 <jirikadlec2@localhost>
2011-09-03 15:05:32 -0600

    Remove nuget.bat (using GetDotSpatial.exe instead)

--- Source/Main/nuget.bat
+++
@@ -1,8 +0,0 @@
-echo "downloading DotSpatial package ... please wait"
-cd %1
-Nuget\nuget.exe install %1HydroDesktop.Main\packages.config -o %1Packages  -s http://hydro10.sdsc.edu/nuget/nuget
-Nuget\nuget.exe install %1HydroDesktop.Controls\packages.config -o %1Packages  -s http://hydro10.sdsc.edu/nuget/nuget
-Nuget\nuget.exe install %1HydroDesktop.WebServices\HydroDesktop.WebServices\packages.config -o %1Packages  -s http://hydro10.sdsc.edu/nuget/nuget
-
-
-
