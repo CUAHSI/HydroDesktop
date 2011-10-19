22c057ff85fa8de3dc156329aa822819600f0a1b
jirikadlec2 <jirikadlec2@localhost>
2011-10-19 02:46:20 -0600

    initial version of ArcGIS online plugin

---
+++ Source/Plugins/ArcGisOnline/ArcGISOnlineForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HydroDesktop.ArcGisOnline
+{
+    public partial class ArcGISOnlineForm : Form
+    {
+        public ArcGISOnlineForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //TODO: validate URL
+            ServiceRequest.GetFeatures(this.textBox1.Text);
+        }
+    }
+}
