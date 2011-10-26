6b9ab94828d23c5f7f293f29ce43a4cf00cdc55a
jirikadlec2 <jirikadlec2@localhost>
2011-10-26 00:33:46 -0600

    GetDotSpatial - simplify the code

--- Source/GetDotSpatial/Downloader.cs
+++ Source/GetDotSpatial/Downloader.cs
@@ -24,12 +24,6 @@ namespace GetDotSpatial
         static void Main(string[] args)
         {
             DeleteOldAssemblies();          
-            UpdateExternalFiles();
-        }
-
-        public static void UpdateExternalFiles()
-        {
-            UpdateDotSpatial();
             UpdatePackages();
         }
 
@@ -171,11 +165,18 @@ namespace GetDotSpatial
                 packageUrl = String.Format("{0}/Package/Download/{1}/{2}", packageSource, packageId, packageVersion);
                 targetFolder = Path.GetDirectoryName(zipFileToDownload);
             }
+            else if (packageId.Contains("DotSpatial_x86"))
+            {
+                //DotSpatial is using a special naming convention for package naming
+                packageUrl = String.Format("{0}/DotSpatial_x86.{1}.nupkg", packageSource, packageVersion);
+                targetFolder = FindTargetFolder(packageTarget);
+            }
             else
             {
                 packageUrl = String.Format("{0}/{1}/{2}", packageSource, packageId, packageVersion);
                 targetFolder = FindTargetFolder(packageTarget);
             }
+
 
             //check if the package file already exists - this also checks the version
             if (File.Exists(zipFileToDownload))
@@ -222,6 +223,9 @@ namespace GetDotSpatial
 
         private static void UnzipNugetPackage(string zipFile, string targetFolder, string packageId, string packageVersion)
         {
+            string[] listOfFilesToUnzip = ReadListOfFilesToUnzip(packageId); //this array is NULL if there are no restrictions
+            bool restrictFiles = (listOfFilesToUnzip != null);
+            
             if (!targetFolder.EndsWith(Path.DirectorySeparatorChar.ToString()))
             {
                 targetFolder += Path.DirectorySeparatorChar.ToString();
@@ -278,14 +282,26 @@ namespace GetDotSpatial
                 bool result;
                 foreach (ZipStorer.ZipFileEntry entry in dir)
                 {
-                    if (copyToBinaries) //only copy dll, pdb and xml to binaries
+                    if (copyToBinaries) //select the files which should be copied to /Binaries folder
                     {
                         path = Path.Combine(targetFolder, Path.GetFileName(entry.FilenameInZip));
                         string ext = Path.GetExtension(path).ToLower();
-                        if (entry.FilenameInZip.StartsWith("lib"))
+
+                        if (!restrictFiles) //don't restrict files
                         {
-                            result = zip.ExtractFile(entry, path);
-                            Console.WriteLine(path + (result ? "" : " (error)"));
+                            if (entry.FilenameInZip.StartsWith("lib"))
+                            {
+                                result = zip.ExtractFile(entry, path);
+                                Console.WriteLine(path + (result ? "" : " (error)"));
+                            }
+                        }
+                        else //restrict files
+                        {
+                            if (listOfFilesToUnzip.Contains(Path.GetFileName(entry.FilenameInZip)))
+                            {
+                                result = zip.ExtractFile(entry, path);
+                                Console.WriteLine(path + (result ? "" : " (error)"));
+                            }
                         }
                     }
                     else
@@ -366,51 +382,28 @@ namespace GetDotSpatial
             }
         }
 
-        /// <summary>
-        /// Updates DotSpatial Dll's to the version specified in dotspatial_version.txt
-        /// </summary>
-        public static void UpdateDotSpatial()
-        {
-            try
-            {
-                //read version number from the text file
-                string DotSpatialVersion = ReadDotSpatialVersion();
-                
-                //download the DotSpatial .nupkg zip file
-                string DotSpatialZipFile = DownloadDotSpatial(DotSpatialVersion);
-                
-                //unzip the DotSpatial .nupkg file to the specified folder
-                UnzipDotSpatialFolder(DotSpatialZipFile);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-
-
-        public static string ReadDotSpatialVersion()
-        {
-            string versionNumberFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, versionNumberTextFile);
-            
-            if (!File.Exists(versionNumberFilePath)) throw new FileNotFoundException(Path.Combine("The file {0} doesn't exist.", versionNumberFilePath));
-
-            using (StreamReader r = new StreamReader(versionNumberFilePath))
-            {
-                string line = r.ReadLine();
-                return line;
-            }
-        }
-
         public static string[] ReadListOfFilesToDelete()
         {
             string deleteFilesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, deleteFilesTextFile);
 
-            if (!File.Exists(deleteFilesPath)) throw new FileNotFoundException(Path.Combine("The file {0} doesn't exist.", deleteFilesPath));
+            return readLinesFromFile(deleteFilesPath);
+        }
+
+        public static string[] ReadListOfFilesToUnzip(string packageId)
+        {
+            string unzipFilesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, packageId + "_files.txt");
+
+            if (!File.Exists(unzipFilesPath)) return null;
+
+            return readLinesFromFile(unzipFilesPath);
+        }
+
+        private static string[] readLinesFromFile(string fileName)
+        {
+            if (!File.Exists(fileName)) throw new FileNotFoundException(Path.Combine("The file {0} doesn't exist.", fileName));
 
             List<string> lines = new List<string>();
-            using (StreamReader r = new StreamReader(deleteFilesPath))
+            using (StreamReader r = new StreamReader(fileName))
             {
                 // Use while != null pattern for loop
                 string line;
@@ -421,153 +414,6 @@ namespace GetDotSpatial
                 }
             }
             return lines.ToArray();
-        }
-
-        /// <summary>
-        /// Downloads the DotSpatial .zip file to the local folder
-        /// </summary>
-        /// <param name="teamCityBuildNumber">DotSpatial "team city" build number</param>
-        /// <param name="productVersion">DotSpatial product version</param>
-        public static string DownloadDotSpatial(string productVersion)
-        {
-            //generate the URL
-            string url = String.Format(@"http://hydro10.sdsc.edu/nuget/Packages/DotSpatial_x86.{0}.nupkg", productVersion);
-            
-            //find the 'Packages' folder
-            string packagesFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Packages");
-
-            //generate name of the .nupkg zip file
-            string zipFileToDownload = Path.Combine(packagesFolder, String.Format("DotSpatial_x86.{0}{1}DotSpatial_x86.{0}.nupkg", productVersion, Path.DirectorySeparatorChar));
-            
-            //only re-download packages file if it exists
-            if (File.Exists(zipFileToDownload))
-            {
-                Console.WriteLine(String.Format("Package file {0} is already downloaded.", zipFileToDownload));
-                return zipFileToDownload;
-            }
-
-            //check the download directory
-            string downloadDirectory = Path.GetDirectoryName(zipFileToDownload);            
-            if (!Directory.Exists(downloadDirectory))
-            {
-                try
-                {
-                    Directory.CreateDirectory(downloadDirectory);
-                }
-                catch (Exception ex)
-                {
-                    throw new UnauthorizedAccessException("Unable to create directory " + downloadDirectory);
-                }
-            }
-
-            //download the .nupkg file
-            try
-            {
-                using (WebClient wc = new WebClient())
-                {
-                    Console.WriteLine("Downloading " + url + " ...");
-                    wc.DownloadFile(url, zipFileToDownload);
-                    return zipFileToDownload;
-                }
-            } 
-            catch (WebException ex1)
-            {
-                throw new WebException("Error downloading package " + url + " - " + ex1.Message);
-            }
-            catch(UnauthorizedAccessException ex2)
-            {
-                throw new WebException("Access denied to folder " + Path.GetDirectoryName(zipFileToDownload) + " - " + ex2.Message);
-            }
-        }
-
-        public static void UnzipDotSpatialFolder(string zipFile)
-        {
-            // Create DotSpatial Packages folder
-            string targetFolder = Path.Combine(Path.GetDirectoryName(zipFile), "lib");
-            if (!Directory.Exists(targetFolder))
-                Directory.CreateDirectory(targetFolder);
-            
-            // Previous checkings
-            if (string.IsNullOrEmpty(zipFile))
-            {
-                throw new ArgumentException("Unzip DotSpatial - .nupkg filename not defined.");
-            }
-            if (string.IsNullOrEmpty(targetFolder))
-            {
-                throw new ArgumentException("Unzip DotSpatial - Target folder not defined.");
-            }
-            if (!Directory.Exists(targetFolder))
-                throw new IOException("Directory " + targetFolder + " doesn't exist.");
-
-            // check if the Dll's are already present. If all dll's are found, don't unzip the files.
-            string[] requiredAssemblies = new string[] 
-            {
-                "DotSpatial.Analysis",
-                "DotSpatial.Controls",
-                "DotSpatial.Data",
-                "DotSpatial.Data.Forms",
-                "DotSpatial.Extensions",
-                "DotSpatial.Modeling",
-                "DotSpatial.Modeling.Forms",
-                "DotSpatial.Projections",
-                "DotSpatial.Projections.Forms",
-                "DotSpatial.Serialization",
-                "DotSpatial.Symbology",
-                "DotSpatial.Symbology.Forms",
-                "DotSpatial.Topology"
-            };
-            bool assembliesExist = true;
-            foreach (string assemblyName in requiredAssemblies)
-            {
-                string fullPath = Path.Combine(targetFolder, assemblyName + ".dll");
-                if (!File.Exists(fullPath))
-                {
-                    assembliesExist = false;
-                    break;
-                }
-            }
-
-            if (assembliesExist)
-            {
-                Console.WriteLine(String.Format("The Zip File {0} is already unzipped.",zipFile));
-                return;
-            }
-
-            try
-            {
-                // Opens existing zip file
-                ZipStorer zip = ZipStorer.Open(zipFile, FileAccess.Read);
-
-                // Read all directory contents
-                List<ZipStorer.ZipFileEntry> dir = zip.ReadCentralDir();
-
-                // Extract all files in target directory
-                string path;
-                bool result;
-                foreach (ZipStorer.ZipFileEntry entry in dir)
-                {
-                    path = Path.Combine(targetFolder, Path.GetFileName(entry.FilenameInZip));
-
-                    //only extract required assemblies
-                    string fileNameWithoutExt = Path.GetFileNameWithoutExtension(path);
-                    if (requiredAssemblies.Contains(fileNameWithoutExt))
-                    {
-                        result = zip.ExtractFile(entry, path);
-                        Console.WriteLine(path + (result ? "" : " (error)"));
-                    }
-                }
-                zip.Close();
-
-                Console.WriteLine("Unzip DotSpatial - Source file processed with success.");
-            }
-            catch (InvalidDataException)
-            {
-                Console.WriteLine("Unzip DotSpatial Error: Invalid or not supported Zip file.");
-            }
-            catch
-            {
-                Console.WriteLine("Unzip DotSpatial Error while processing source file.");
-            }
         }
     }
 }
