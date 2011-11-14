19dbd227a27ad4a8f31f61893db33f5680cff19c
jirikadlec2 <jirikadlec2@localhost>
2011-11-14 11:23:05 -0700

    updating all information in AssemblyInfo.cs files

--- Source/HydroDesktop.MainPlugin/Properties/AssemblyInfo.cs
+++ Source/HydroDesktop.MainPlugin/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HydroDesktop.MainPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HydroDesktop Main Entry Point Extension")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CUAHSI")]
 [assembly: AssemblyProduct("HydroDesktop.MainPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.124")]
-[assembly: AssemblyFileVersion("1.3.124")]
+[assembly: AssemblyVersion("1.3.134")]
+[assembly: AssemblyFileVersion("1.3.134")]
