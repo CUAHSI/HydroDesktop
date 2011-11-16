3f0419f64374d9d2dd6e99f15d9b59ae43ab9aad
jirikadlec2 <jirikadlec2@localhost>
2011-11-16 03:24:30 -0700

    Adding initial version of FacetedSearch

---
+++ Source/FacetedSearch/Searching/SearchLayerCreator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using DotSpatial.Controls;
+using DotSpatial.Data;
+using DotSpatial.Symbology;
+using System.Linq;
+
+namespace FacetedSearch.Searching
+{
+    class SearchLayerCreator
+    {
+        #region Fields
+
+        private readonly IMap _map;
+        private readonly SearchResult _searchResult;
+        private readonly string _searchGroupName;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor of <see cref="SearchLayerCreator"/>
+        /// </summary>
+        /// <param name="map">Map</param>
+        /// <param name="searchResult">Search result</param>
+        /// <param name="searchGroupName">Name of search layers group.</param>
+        public SearchLayerCreator(IMap map, SearchResult searchResult, string searchGroupName)
+        {
+            if (map == null) throw new ArgumentNullException("map");
+            if (searchResult == null) throw new ArgumentNullException("searchResult");
+            if (searchGroupName == null) throw new ArgumentNullException("searchGroupName");
+
+            _map = map;
+            _searchResult = searchResult;
+            _searchGroupName = searchGroupName;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Create search layer
+        /// </summary>
+        public void Create()
+        {
+            var root = GetSearchResultLayerGroup() ?? new MapGroup(_map, _searchGroupName);
+            foreach(var item in _searchResult.ResultItems)
+            {
+                var subResultLayer = CreateSearchResultLayer(item, root);
+                root.Add(subResultLayer);
+            }
+            _map.Refresh();
+
+            //assign the projection again
+            foreach (var item in _searchResult.ResultItems)
+                item.FeatureSet.Reproject(_map.Projection);
+
+            for (int i = 0; i < root.Layers.Count; i++)
+            {
+                var layer = root[i];
+                var state = i == 0;
+                var rendItem = layer as IRenderableLegendItem;
+                if (rendItem != null)
+                {
+                    rendItem.IsVisible = state; // force a re-draw in the case where we are talking about layers.
+                }
+                else
+                {
+                    layer.Checked = state;
+                }
+            }
+
+            _map.Refresh();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private MapPointLayer CreateSearchResultLayer(SearchResultItem item, MapGroup root)
+        {
+            var myLayer = new MapPointLayer(item.FeatureSet);
+
+            // Get Title of web-server
+            var webService = Settings.SearchSettings.Instance.WebServicesSettings.WebServices.FirstOrDefault(
+                ws => ws.ServiceCode == item.SeriesDataCart.ServCode);
+            var defaulLegendText = webService != null? webService.Title : item.SeriesDataCart.ServCode;
+
+            // Build legend text 
+            var legendText = defaulLegendText;
+            int nameIndex = 1;
+            while(true)
+            {
+                // Check if legend text is already used
+                var nameUsed = root.Layers.Any(layer => layer.LegendText == legendText);
+                if (!nameUsed)
+                {
+                    break;
+                }
+
+                // Create new legend text
+                nameIndex++;
+                legendText = string.Format("{0} ({1})", defaulLegendText, nameIndex);
+            }
+
+            myLayer.LegendText = legendText;
+            return myLayer;
+        }
+        
+        private MapGroup GetSearchResultLayerGroup()
+        {
+            MapGroup layer = null;
+            foreach (var lay in _map.Layers)
+            {
+                if (lay is MapGroup &&
+                    lay.LegendText.ToLower() == _searchGroupName.ToLower())
+                {
+                    layer = lay as MapGroup;
+                    break;
+                }
+            }
+            return layer;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/FacetedSearch/Settings/AreaRectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FacetedSearch.Settings
+{
+    public class AreaRectangle
+    {
+        public AreaRectangle(double xmin, double ymin, double xmax, double ymax)
+        {
+            XMin = xmin;
+            YMin = ymin;
+            XMax = xmax;
+            YMax = ymax;
+        }
+
+        public double XMin { get; private set; }
+        public double XMax { get; private set; }
+        public double YMin { get; private set; }
+        public double YMax { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("Point1 (Lng/Lat): {0:N6} {1:N6}" + Environment.NewLine +
+                                 "Point2 (Lng/Lat): {2:N6} {3:N6}", XMin, YMin, YMax, YMax);
+        }
+    }
+}
