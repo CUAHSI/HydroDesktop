89694fe2373362168d8a48a1f2e25a4ec9a772fd
mogikanin <mogikanin.tir@gmail.com>
2011-12-18 14:11:27 +0400

    Fixed references in unit-tests projects

--- Source/DataDownload/DataDownloadTests/Properties/AssemblyInfo.cs
+++ Source/DataDownload/DataDownloadTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataDownloadTests")]
+[assembly: AssemblyTitle("DataDownload.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataDownloadTests")]
+[assembly: AssemblyProduct("DataDownload.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
