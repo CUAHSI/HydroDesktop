1867163fa96e084bcae7637ccabda8176db1b3c9
mogikanin <mogikanin.tir@gmail.com>
2012-01-28 17:33:07 +0400

    Fixed #8352: Clicking on Search Tab goes to Map Tab

--- Source/HydroDesktop.MainPlugin/HydroDesktopMainPlugin.cs
+++ Source/HydroDesktop.MainPlugin/HydroDesktopMainPlugin.cs
@@ -325,7 +325,7 @@
 
                 //if the clicked root item was 'search', then don't select the map root item
                 //(the user intended to show search tab and map panel)
-                if (App.SerializationManager.GetCustomSetting<bool>("SearchRootClicked", false))
+                if (!App.SerializationManager.GetCustomSetting("SearchRootClicked", false))
                 {
                     App.HeaderControl.SelectRoot(HeaderControl.HomeRootItemKey);
                 }
