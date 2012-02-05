0f6525a040a0d8a3deaa9b3bc4ee14ba7707c32a
mogikanin <mogikanin.tir@gmail.com>
2012-02-05 22:17:27 +0400

    Added preview for excel files.

--- Source/DataImport/Excel/ExcelImporter.cs
+++ Source/DataImport/Excel/ExcelImporter.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
+using System.Linq;
+using Excel;
 using Wizard.UI;
 
 namespace DataImport.Excel
@@ -22,7 +25,7 @@ namespace DataImport.Excel
 
         public IDataImportSettings GetDefaultSettings()
         {
-            throw new NotImplementedException();
+            return new ExcelImportSettings();
         }
 
         public void Import(IDataImportSettings settings)
@@ -32,12 +35,64 @@ namespace DataImport.Excel
 
         public ICollection<Func<DataImportContext, WizardPage>> GePageCreators()
         {
-            throw new NotImplementedException();
+            return new Collection<Func<DataImportContext, WizardPage>>
+                       {
+                           c => new FormatOptionsPage(c),
+                       };
         }
 
         public DataTable GetPreview(IDataImportSettings settings)
         {
-            throw new NotImplementedException();
+            var excelSettings = (ExcelImportSettings)settings;
+            return excelSettings.DataSet.Tables[excelSettings.SheetName];
         }
+
+        public DataSet AsDataSet(IDataImportSettings settings)
+        {
+            var excelSettings = (ExcelImportSettings)settings;
+
+            var fileName = excelSettings.PathToFile;
+            if (!File.Exists(fileName))
+            {
+                return new DataSet();
+            }
+
+            var extension = Path.GetExtension(fileName);
+            IExcelDataReader reader = null;
+            var stream = File.Open(fileName, FileMode.Open);
+            try
+            {
+                switch (extension)
+                {
+                    case ".xls":
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        break;
+                    case ".xlsx":
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                        break;
+                    default:
+                        goto case ".xls";
+                }
+
+                reader.IsFirstRowAsColumnNames = true;
+                var ds = reader.AsDataSet();
+                return ds;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                stream.Close();
+            }
+        }
+    }
+
+    public class ExcelImportSettings : IDataImportSettings
+    {
+        public string PathToFile {get;set;}
+        public string SheetName { get; set; }
+        public DataSet DataSet { get; set; }
     }
 }
