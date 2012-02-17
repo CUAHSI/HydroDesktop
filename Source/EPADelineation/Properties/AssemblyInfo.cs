2af98bfe53b937882129d8766607b0436ca5f15e
jirikadlec2 <jirikadlec2@localhost>
2012-02-17 11:34:47 -0700

    Merge with default

--- Source/EPADelineation/Properties/AssemblyInfo.cs
+++ Source/EPADelineation/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EPA Delineation")]
-[assembly: AssemblyDescription("EPA Watershed Delineation")]
+[assembly: AssemblyDescription("EPA Watershed Delineation - Delineate any watershed in the U.S")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jingqi Dong")]
 [assembly: AssemblyProduct("EPA Delineation")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
