b1782826f280c5b26f16a476bc394fd105869e3d
jirikadlec2 <jirikadlec2@localhost>
2012-02-21 17:25:09 -0700

    Fixed bug 8416 (file-new doesn't reset form title and db path)

--- Source/HydroDesktop.MainPlugin/HydroDesktopMainPlugin.cs
+++ Source/HydroDesktop.MainPlugin/HydroDesktopMainPlugin.cs
@@ -63,7 +63,7 @@
             //write the log file from trace
             Trace.Flush();
             
-            var projectExists = App.SerializationManager.CurrentProjectFile != null;
+            var projectExists = (!String.IsNullOrEmpty(App.SerializationManager.CurrentProjectFile));
             if (!projectExists)
             {
                 var res = MessageBox.Show(string.Format("Save changes to new project?"),
@@ -205,7 +205,14 @@
 
         void SerializationManager_NewProjectCreated(object sender, SerializingEventArgs e)
         {
-            //throw new NotImplementedException();
+            SetupDatabases();
+            Shell.Text = HYDRODESKTOP_NAME;
+            //setup new db information
+            //SeriesControl.SetupDatabase();
+            if (App.Map.Projection != null)
+            {
+                latLongDisplay.MapProjectionString = App.Map.Projection.ToEsriString();
+            }
         }
 
         void SerializationManager_Deserializing(object sender, SerializingEventArgs e)
