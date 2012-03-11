2c766e05688a53ea0858dbd5a4c375d134f2c59c
mogikanin <mogikanin.tir@gmail.com>
2012-03-11 19:06:42 +0400

    DAL: Added repository for UnitConversion.

---
+++ Source/HydroDesktop.Interfaces/IUnitConversionsRepository.cs
@@ -0,0 +1,11 @@
+using HydroDesktop.Interfaces.ObjectModel;
+
+namespace HydroDesktop.Interfaces
+{
+    /// <summary>
+    /// Interface for <see cref="UnitConversion"/> Repository
+    /// </summary>
+    public interface IUnitConversionsRepository : IRepository<UnitConversion>
+    {
+    }
+}
--- Source/HydroDesktop.Interfaces/ObjectModel/Unit.cs
+++ Source/HydroDesktop.Interfaces/ObjectModel/Unit.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace HydroDesktop.Interfaces.ObjectModel
+﻿namespace HydroDesktop.Interfaces.ObjectModel
 {
     /// <summary>
     /// Represents an observation unit or a time unit
@@ -12,7 +9,9 @@ namespace HydroDesktop.Interfaces.ObjectModel
         /// Creates a new default instance of the unit object
         /// </summary>
         public Unit()
-        { }
+        {
+        }
+
         /// <summary>
         /// Creates an instance of a unit object
         /// </summary>
@@ -25,18 +24,37 @@ namespace HydroDesktop.Interfaces.ObjectModel
             UnitsType = type;
             Abbreviation = abbreviation;
         }
+
         /// <summary>
         /// The unit name (for example "centimeter")
         /// </summary>
         public virtual string Name { get; set; }
+
         /// <summary>
         /// The unit type ("length", "time")
         /// </summary>
         public virtual string UnitsType { get; set; }
+
         /// <summary>
         /// The unit abbreviation
         /// </summary>
         public virtual string Abbreviation { get; set; }
+
+        /// <summary>
+        /// Dimension of unit
+        /// </summary>
+        public virtual string Dimension { get; set; }
+
+        /// <summary>
+        /// Conversion factor to base SI unit
+        /// </summary>
+        public virtual double? ConversionFactorToSI { get; set; }
+
+        /// <summary>
+        /// Offset to base SI unit
+        /// </summary>
+        public virtual double? OffsetToSI { get; set; }
+
         /// <summary>
         /// Converts the unit to string
         /// </summary>
@@ -54,11 +72,11 @@ namespace HydroDesktop.Interfaces.ObjectModel
             get
             {
                 return new Unit
-                {
-                    Name = Constants.Unknown,
-                    UnitsType = Constants.Unknown,
-                    Abbreviation = Constants.Unknown
-                };
+                           {
+                               Name = Constants.Unknown,
+                               UnitsType = Constants.Unknown,
+                               Abbreviation = Constants.Unknown
+                           };
             }
         }
 
@@ -70,15 +88,16 @@ namespace HydroDesktop.Interfaces.ObjectModel
             get
             {
                 return new Unit
-                {
-                    Name = Constants.Unknown,
-                    UnitsType = "Time",
-                    Abbreviation = Constants.Unknown
-                };
+                           {
+                               Name = Constants.Unknown,
+                               UnitsType = "Time",
+                               Abbreviation = Constants.Unknown
+                           };
             }
         }
 
         #region Equality
+
         /// <summary>
         /// Two units are considered equal, if they have the same name, abbreviation and unit type
         /// </summary>
@@ -88,13 +107,14 @@ namespace HydroDesktop.Interfaces.ObjectModel
         {
             if (other is Unit)
             {
-                if (((Unit)other).Name != Name) return false;
-                if (((Unit)other).UnitsType != UnitsType) return false;
-                if (((Unit)other).Abbreviation != Abbreviation) return false;
+                if (((Unit) other).Name != Name) return false;
+                if (((Unit) other).UnitsType != UnitsType) return false;
+                if (((Unit) other).Abbreviation != Abbreviation) return false;
                 return true;
             }
             return base.Equals(other);
         }
+
         /// <summary>
         /// Returns an unique hash code of the unit object
         /// </summary>
@@ -103,6 +123,7 @@ namespace HydroDesktop.Interfaces.ObjectModel
         {
             return (Name + "|" + UnitsType + "|" + Abbreviation).GetHashCode();
         }
+
         #endregion
     }
 }
