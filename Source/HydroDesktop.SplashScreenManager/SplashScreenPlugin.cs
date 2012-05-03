45a0f5220285846732a95beb1b34f11f522534ef
jirikadlec2 <jirikadlec2@localhost>
2012-05-02 19:14:22 -0600

    using a custom splash screen manager for HydroDesktop (this doesn't require referencing the DevXpress files)

---
+++ Source/HydroDesktop.SplashScreenManager/SplashScreenPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using DotSpatial.Extensions.SplashScreens;
+
+namespace HydroDesktop.SplashScreenManager
+{
+    public class SplashScreenPlugin : ISplashScreenManager
+    {
+        public void ProcessCommand(Enum cmd, object arg)
+        {
+            //if (SplashScreenManager.Default != null)
+            //    SplashScreenManager.Default.SendCommand(cmd, arg);
+            SplashScreen.UdpateStatusText(arg.ToString());
+        }
+
+        public void Activate()
+        {
+            SplashScreen.ShowSplashScreen();
+        }
+
+        public void Deactivate()
+        {
+            SplashScreen.CloseSplashScreen();
+        }
+    }
+}
