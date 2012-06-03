4b855774c4584f3c6e34af8b71ed68eb87f4f980
mogikanin <mogikanin.tir@gmail.com>
2012-06-03 17:31:49 +0400

    Fixed WaterOneFlow parser error for NWIS Daily Values (#8482)

--- Source/HydroDesktop.WebServices.Tests/WaterOneFlow/WaterOneFlow10ParserTests.cs
+++ Source/HydroDesktop.WebServices.Tests/WaterOneFlow/WaterOneFlow10ParserTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using HydroDesktop.WebServices.WaterOneFlow;
-using HydroDesktop.Interfaces.ObjectModel;
 using NUnit.Framework;
 using System.IO;
 
@@ -13,20 +11,15 @@ namespace HydroDesktop.WebServices.Tests.WaterOneFlow
         [TestCase(@"TestFiles\v10\")]
         public void TestParseDataSeries(String samplesPath)
         {
-            IWaterOneFlowParser parser = new WaterOneFlow10Parser();
-            string xmlDirectory = GetXmlTempDirectory() ;
-
-            // we want a clean set of test cases
-            CleanAndCopySamplesFiles(xmlDirectory, samplesPath);
-
-            string[] fileNames = Directory.GetFiles(xmlDirectory);
-
-            foreach (string fileName in fileNames)
+            var target = new WaterOneFlow10Parser();
+            foreach (var fileName in Directory.GetFiles(samplesPath))
             {
-                IList<Series> seriesList = parser.ParseGetValues(fileName);
+                var seriesList = target.ParseGetValues(fileName);
+
                 Assert.Greater(seriesList.Count, 0);
-                Assert.Greater(seriesList[0].ValueCount, 0, 
-                    "Error in filename '"+fileName + "' the number of values in the seriesList must be greater than zero.");
+                Assert.Greater(seriesList[0].ValueCount, 0,
+                               "Error in filename '" + fileName +
+                               "' the number of values in the seriesList must be greater than zero.");
             }
         }
 
@@ -34,91 +27,13 @@ namespace HydroDesktop.WebServices.Tests.WaterOneFlow
         [TestCase(@"TestFiles\v10sites\")]
         public void TestParseGetSites(String samplesPath)
         {
-            IWaterOneFlowParser parser = new WaterOneFlow10Parser();
-            string xmlDirectory = GetXmlTempDirectory();
-
-            // we want a clean set of test cases
-            CleanAndCopySamplesFiles(xmlDirectory, samplesPath);
-
-            string[] fileNames = Directory.GetFiles(xmlDirectory);
-
-            foreach (string fileName in fileNames)
+            var target = new WaterOneFlow10Parser();
+            foreach (var fileName in Directory.GetFiles(samplesPath))
             {
-                IList<Site> sites = parser.ParseGetSites(fileName);
-                Assert.Greater(sites.Count, 0, "Error in file '"+fileName+ "' the number of sites must be greater than zero.");
+                var sites = target.ParseGetSites(fileName);
+                Assert.Greater(sites.Count, 0,
+                               "Error in file '" + fileName + "' the number of sites must be greater than zero.");
             }
-
-        }
-
-        private void CleanAndCopySamplesFiles(string xmlDirectory, string samplesPath)
-        {
-            foreach (var file in Directory.GetFiles(xmlDirectory))
-            {
-                File.Delete(file);
-            }
-            foreach (var file in Directory.GetFiles(samplesPath))
-            {
-                string fileFrom = AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar +
-                                  file;
-                string fileTo = Path.Combine(xmlDirectory, Path.GetFileName(file));
-                File.Copy(fileFrom, fileTo, true);
-            }
-        }
-
-
-        public void TestParseGetSiteInfo()
-        {
-            IWaterOneFlowParser parser = new WaterOneFlow10Parser();
-            string xmlDirectory = GetXmlTempDirectory();
-           
-
-            string[] fileNames = Directory.GetFiles(xmlDirectory);
-
-            List<SeriesMetadata> fullSeriesList = new List<SeriesMetadata>();
-
-            foreach (string path in fileNames)
-            {
-                string fileName = Path.GetFileName(path);
-                if (fileName.ToLower().StartsWith("site-"))
-                {
-                    IList<SeriesMetadata> seriesList = parser.ParseGetSiteInfo(path);
-                    fullSeriesList.AddRange(seriesList);
-                    //Assert.Greater(seriesList.Count, 0, 
-                        //"the number of series in the series list should be > zero." );
-                }
-            }
-            Assert.Greater(fullSeriesList.Count, 0, "the full series list count should be > 0");
-        }
-
-        /// <summary>
-        /// Gets the temporary directory for xml files downloaded
-        /// by HydroDesktop
-        /// </summary>
-        /// <returns>the directory path</returns>
-        public static string GetXmlTempDirectory()
-        {
-            //Check if we need to create a temporary folder for storing the xml file
-            string tempDirectory = Path.Combine(Path.GetTempPath(), @"HydroDesktop\UnitTestFiles");
-            if (Directory.Exists(tempDirectory) == false)
-            {
-                try
-                {
-                    Directory.CreateDirectory(tempDirectory);
-                }
-                catch
-                {
-                    tempDirectory = Path.GetTempPath();
-                }
-            }
-            return tempDirectory;
-        }
-
-
-        public static string GetXmlFileName()
-        {
-            //return Path.Combine(GetXmlTempDirectory(), "NCDCISD-72586724133-NCDCISD-TMP.xml");
-            //return Path.Combine(GetXmlTempDirectory(), "download0010.xml");
-            return Path.Combine(GetXmlTempDirectory(), "sites.xml");
         }
     }
 }
