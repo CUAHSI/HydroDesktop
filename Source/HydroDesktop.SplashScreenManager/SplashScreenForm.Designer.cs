e5ad312e9e9c69fd2df8fb5f494134a83e0b9afb
jirikadlec2 <jirikadlec2@localhost>
2012-08-18 17:03:40 +0300

    re-enable including 3rd party extensions and sample projects as part of the installer. refactoring of WelcomeScreen (moved sample project detection to separate files) added icon to SplashScreenManager. Fixed bug 8520 by including PowerPacks.vs in support files

--- Source/HydroDesktop.SplashScreenManager/SplashScreenForm.Designer.cs
+++ Source/HydroDesktop.SplashScreenManager/SplashScreenForm.Designer.cs
@@ -83,6 +83,7 @@
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashScreenForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreenForm";
