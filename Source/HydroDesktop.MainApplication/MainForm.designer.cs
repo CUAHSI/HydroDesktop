091af451146f1637e3d564f224abcd20204fc4d2
jirikadlec2 <jirikadlec2@localhost>
2012-08-19 22:13:52 +0300

    SampleProjectInstaller - try to create the AppManager.AbsolutePathToExtensions folder if this folder doesn't exist

--- Source/HydroDesktop.MainApplication/MainForm.designer.cs
+++ Source/HydroDesktop.MainApplication/MainForm.designer.cs
@@ -53,7 +53,7 @@ namespace HydroDesktop.MainApplication
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1040, 712);
+            this.ClientSize = new System.Drawing.Size(1040, 662);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "HydroDesktop";
