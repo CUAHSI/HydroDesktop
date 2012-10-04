e209eb12938bcb00f9890d3c74545dd8e04b9e39
mogikanin <mogikanin.tir@gmail.com>
2012-10-04 23:20:45 +0400

    Added AssemblyInfoShared.cs(vb) that contains shared assembly info for all projects. Set assembly version to 1.5.8.0.

--- Source/HydroDesktop.Data.ODMCVServiceClient/Properties/AssemblyInfo.cs
+++ Source/HydroDesktop.Data.ODMCVServiceClient/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a32e73f6-9d08-41b7-8e3d-4e5a3bac284c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
