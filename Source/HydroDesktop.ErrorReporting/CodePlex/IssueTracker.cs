694ba4872ea76f6784d2a535b083ae82c5ee3c8c
mogikanin <mogikanin.tir@gmail.com>
2012-10-29 01:44:32 +0400

    Added auto-posting issues on codeplex.

---
+++ Source/HydroDesktop.ErrorReporting/CodePlex/IssueTracker.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HydroDesktop.ErrorReporting.CodePlex
+{
+    internal class IssueTracker
+    {
+        private readonly string _projectName;
+        private WebBrowser _browser;
+
+        public bool IsSignedIn { get; private set; }
+
+        public IssueTracker(string projectName)
+        {
+            _projectName = projectName;
+        }
+
+        public void SignIn(string userName, string password)
+        {
+            _browser = new WebBrowser("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.151 Safari/535.19");
+            const string loginUrl = @"https://www.codeplex.com/site/login";
+            var page = _browser.Get(loginUrl);
+            var form = HtmlForm.GetForm(page, "aspnetForm");
+
+            foreach (var t in form.Elements)
+            {
+                if (t.Name == null) continue;
+                switch (t.Name.ToLower())
+                {
+                    case "username":
+                        t.Value = userName;
+                        break;
+                    case "password":
+                        t.Value = password;
+                        break;
+                }
+            }
+
+            var doc = _browser.Post(loginUrl, form);
+            // todo: Check that signed in
+
+            IsSignedIn = true;
+        }
+
+        public void CreateIssue(Issue issue)
+        {
+            if (issue == null) throw new ArgumentNullException("issue");
+            if (!IsSignedIn)
+            {
+                throw new Exception("Not signed in into codeplex.");
+            }
+
+            var createIssueUrl = string.Format(@"http://{0}.codeplex.com/WorkItem/Create", _projectName);
+
+            var createIssuePage = _browser.Get(createIssueUrl);
+            var form = HtmlForm.GetForm(createIssuePage, "aspnetForm");
+            var elements = new List<HtmlElement>(form.Elements);
+            form.Elements.Clear();
+
+            var fileName = issue.FileToAttach;
+            byte[] fileData = null;
+            if (!String.IsNullOrEmpty(fileName) && File.Exists(fileName))
+            {
+                var fi = new FileInfo(fileName);
+                if (fi.Length <= 4*1024*1024) // Max 4 MB
+                {
+                    using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                    {
+                        fileData = new byte[fs.Length];
+                        fs.Read(fileData, 0, fileData.Length);
+                    }
+                }
+            }
+            form.Elements.Add(new HtmlElement("__RequestVerificationToken", elements.First(t => t.Name == "__RequestVerificationToken").Value));
+            form.Elements.Add(new HtmlElement("searchsite", "Search all projects"));
+            form.Elements.Add(new HtmlElement("WorkItem.Summary", issue.Summary));
+            form.Elements.Add(new HtmlElement("WorkItem.Description", issue.Description));
+            if (fileData != null)
+            {
+                form.Elements.Add(new HtmlElement("PostedFile_text", @"C:\fakepath\" + Path.GetFileName(fileName)));
+                form.Elements.Add(new HtmlElement("PostedFile", null)
+                    {
+                        IsFile = true,
+                        File = fileData,
+                        ContentType = "application/x-msdownload",
+                        FileName = Path.GetFileName(fileName)
+                    });
+            }
+            else
+            {
+                form.Elements.Add(new HtmlElement("PostedFile_text", null));
+                form.Elements.Add(new HtmlElement("PostedFile", null) {IsFile = true,});
+            }   
+            
+            form.Elements.Add(new HtmlElement("SubscribeCheckBox", "false"));
+            form.Elements.Add(new HtmlElement("EmailSubscriptionTypeList", "1"));
+            form.Elements.Add(new HtmlElement("StopNotificationsCheckBox", "false"));
+            form.Elements.Add(new HtmlElement("SelectedSubscriptionItem", "1"));
+            form.Elements.Add(new HtmlElement("SelectedStatus", "Proposed"));
+            form.Elements.Add(new HtmlElement("SelectedType", "Issue"));
+            form.Elements.Add(new HtmlElement("SelectedPriority", "Low"));
+            form.Elements.Add(new HtmlElement("WorkItem.PlannedForRelease", "Unassigned"));
+            form.Elements.Add(new HtmlElement("WorkItem.AssignedTo", "Unassigned"));
+            form.Elements.Add(new HtmlElement("SelectedComponent", "No Component Selected"));
+            form.Elements.Add(new HtmlElement("WorkItem.Custom", null));
+
+            _browser.Post(createIssueUrl, form, createIssueUrl, true);
+        }
+    }
+}
--- Source/HydroDesktop.ErrorReporting/ErrorReportingForm.Designer.cs
+++ Source/HydroDesktop.ErrorReporting/ErrorReportingForm.Designer.cs
@@ -31,18 +31,22 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorReportingForm));
             this.lblInfo = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.gbCredentials = new System.Windows.Forms.GroupBox();
+            this.linkRegister = new System.Windows.Forms.LinkLabel();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.tbDescribe = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.tbEmail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.tbError = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.hdLink = new System.Windows.Forms.LinkLabel();
             this.btnCopyError = new System.Windows.Forms.Button();
             this.btnZipLog = new System.Windows.Forms.Button();
             this.btnSendError = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.gbCredentials.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblInfo
@@ -51,7 +55,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblInfo.Location = new System.Drawing.Point(12, 9);
             this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(372, 29);
+            this.lblInfo.Size = new System.Drawing.Size(410, 29);
             this.lblInfo.TabIndex = 0;
             this.lblInfo.Text = "HydroDesktop has encountered an error. We are sorry for the inconvenience.";
             // 
@@ -61,23 +65,86 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.gbCredentials);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.tbDescribe);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.tbEmail);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.tbError);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.hdLink);
             this.panel1.Location = new System.Drawing.Point(15, 41);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(366, 381);
+            this.panel1.Size = new System.Drawing.Size(404, 381);
             this.panel1.TabIndex = 1;
+            // 
+            // gbCredentials
+            // 
+            this.gbCredentials.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbCredentials.Controls.Add(this.linkRegister);
+            this.gbCredentials.Controls.Add(this.tbLogin);
+            this.gbCredentials.Controls.Add(this.label3);
+            this.gbCredentials.Controls.Add(this.label1);
+            this.gbCredentials.Controls.Add(this.tbPassword);
+            this.gbCredentials.Location = new System.Drawing.Point(15, 278);
+            this.gbCredentials.Name = "gbCredentials";
+            this.gbCredentials.Size = new System.Drawing.Size(370, 89);
+            this.gbCredentials.TabIndex = 13;
+            this.gbCredentials.TabStop = false;
+            this.gbCredentials.Text = "Codeplex";
+            // 
+            // linkRegister
+            // 
+            this.linkRegister.AutoSize = true;
+            this.linkRegister.Location = new System.Drawing.Point(271, 22);
+            this.linkRegister.Name = "linkRegister";
+            this.linkRegister.Size = new System.Drawing.Size(79, 13);
+            this.linkRegister.TabIndex = 13;
+            this.linkRegister.TabStop = true;
+            this.linkRegister.Text = "Not registered?";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbLogin.Location = new System.Drawing.Point(69, 19);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(178, 20);
+            this.tbLogin.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Login:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbPassword.Location = new System.Drawing.Point(69, 45);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(178, 20);
+            this.tbPassword.TabIndex = 11;
             // 
             // label6
             // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 269);
+            this.label6.Location = new System.Drawing.Point(12, 169);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(300, 13);
             this.label6.TabIndex = 8;
@@ -85,37 +152,20 @@
             // 
             // tbDescribe
             // 
-            this.tbDescribe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.tbDescribe.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbDescribe.Location = new System.Drawing.Point(15, 288);
+            this.tbDescribe.Location = new System.Drawing.Point(15, 188);
             this.tbDescribe.Multiline = true;
             this.tbDescribe.Name = "tbDescribe";
             this.tbDescribe.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbDescribe.Size = new System.Drawing.Size(334, 80);
+            this.tbDescribe.Size = new System.Drawing.Size(372, 84);
             this.tbDescribe.TabIndex = 7;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 221);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(145, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Your email address (optional):";
-            // 
-            // tbEmail
-            // 
-            this.tbEmail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbEmail.Location = new System.Drawing.Point(17, 237);
-            this.tbEmail.Name = "tbEmail";
-            this.tbEmail.Size = new System.Drawing.Size(332, 20);
-            this.tbEmail.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 103);
+            this.label4.Location = new System.Drawing.Point(12, 44);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 4;
@@ -125,12 +175,12 @@
             // 
             this.tbError.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbError.Location = new System.Drawing.Point(17, 119);
+            this.tbError.Location = new System.Drawing.Point(15, 60);
             this.tbError.Multiline = true;
             this.tbError.Name = "tbError";
             this.tbError.ReadOnly = true;
             this.tbError.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tbError.Size = new System.Drawing.Size(332, 94);
+            this.tbError.Size = new System.Drawing.Size(370, 94);
             this.tbError.TabIndex = 3;
             // 
             // label2
@@ -139,27 +189,15 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(335, 69);
+            this.label2.Size = new System.Drawing.Size(373, 39);
             this.label2.TabIndex = 1;
             this.label2.Text = "We have created an error report that you can send to us by clicking the “Send Err" +
-    "or” button below.\r\n\r\nAlternatively you may use the online issue tracker to enter" +
-    " your error here:";
-            // 
-            // hdLink
-            // 
-            this.hdLink.AutoSize = true;
-            this.hdLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.hdLink.Location = new System.Drawing.Point(18, 78);
-            this.hdLink.Name = "hdLink";
-            this.hdLink.Size = new System.Drawing.Size(329, 17);
-            this.hdLink.TabIndex = 0;
-            this.hdLink.TabStop = true;
-            this.hdLink.Text = "http://hydrodesktop.codeplex.com/Wo​rkItem/Create";
+    "or” button below.";
             // 
             // btnCopyError
             // 
-            this.btnCopyError.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCopyError.Location = new System.Drawing.Point(100, 428);
+            this.btnCopyError.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCopyError.Location = new System.Drawing.Point(16, 428);
             this.btnCopyError.Name = "btnCopyError";
             this.btnCopyError.Size = new System.Drawing.Size(78, 37);
             this.btnCopyError.TabIndex = 2;
@@ -169,8 +207,8 @@
             // 
             // btnZipLog
             // 
-            this.btnZipLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnZipLog.Location = new System.Drawing.Point(184, 428);
+            this.btnZipLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnZipLog.Location = new System.Drawing.Point(100, 428);
             this.btnZipLog.Name = "btnZipLog";
             this.btnZipLog.Size = new System.Drawing.Size(75, 37);
             this.btnZipLog.TabIndex = 3;
@@ -181,19 +219,19 @@
             // btnSendError
             // 
             this.btnSendError.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSendError.Enabled = false;
-            this.btnSendError.Location = new System.Drawing.Point(276, 428);
+            this.btnSendError.Location = new System.Drawing.Point(313, 428);
             this.btnSendError.Name = "btnSendError";
             this.btnSendError.Size = new System.Drawing.Size(106, 37);
             this.btnSendError.TabIndex = 4;
             this.btnSendError.Text = "Send Error";
             this.btnSendError.UseVisualStyleBackColor = true;
+            this.btnSendError.Click += new System.EventHandler(this.btnSendError_Click);
             // 
             // ErrorReportingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 475);
+            this.ClientSize = new System.Drawing.Size(432, 475);
             this.Controls.Add(this.btnSendError);
             this.Controls.Add(this.btnZipLog);
             this.Controls.Add(this.btnCopyError);
@@ -210,6 +248,8 @@
             this.Shown += new System.EventHandler(this.ErrorReportingForm_Shown);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.gbCredentials.ResumeLayout(false);
+            this.gbCredentials.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -221,13 +261,16 @@
         private System.Windows.Forms.Button btnCopyError;
         private System.Windows.Forms.Button btnZipLog;
         private System.Windows.Forms.Button btnSendError;
-        private System.Windows.Forms.LinkLabel hdLink;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox tbDescribe;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox tbEmail;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox tbError;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.GroupBox gbCredentials;
+        private System.Windows.Forms.LinkLabel linkRegister;
     }
 }
