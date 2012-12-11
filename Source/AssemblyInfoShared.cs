ba3f84a3dc76feca5d589e3078731003397b85c1
ericch <ericch@localhost>
2012-12-11 10:58:36 -0700

    adding keyword icon

---
+++ Source/AssemblyInfoShared.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+/* 
+ * Shared assembly attributes for all projects 
+ */
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.11.0")]
+[assembly: AssemblyFileVersion("1.5.11.0")]
---
+++ Source/HydroDesktop.MainApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using HydroDesktop.Common;
+using HydroDesktop.Common.Logging;
+using HydroDesktop.ErrorReporting;
+
+namespace HydroDesktop.MainApplication
+{
+    static class Program
+    {
+        private static MainForm _mainForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var log = AppContext.Instance.Get<ILog>();
+            Application.ApplicationExit += delegate
+                {
+                    log.Info("Application Exit");
+                    AppContext.Instance.Dispose();
+                };
+            // Log all unhandled exceptions
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += (sender, e) => ProcessUnhandled(e.Exception, false);
+            AppDomain.CurrentDomain.UnhandledException +=
+                    delegate(object sender, UnhandledExceptionEventArgs e)
+                        {
+                            ProcessUnhandled((Exception)e.ExceptionObject, true);
+                            AppContext.Instance.Dispose();
+                        };
+            
+            log.Info("Application Started");
+            _mainForm = new MainForm();
+            if (args.Length > 0 && System.IO.File.Exists(args[0]))
+            {
+                _mainForm.appManager.SerializationManager.OpenProject(args[0]);
+            }
+
+            Application.Run(_mainForm);
+        }
+
+        private static void ProcessUnhandled(Exception ex, bool isFatal)
+        {
+            var log = AppContext.Instance.Get<ILog>();
+            log.Error(isFatal ? "Fatal" : "Unhandled", ex);
+            var errorForm = new ErrorReportingForm(new ErrorReportingFormArgs
+                {
+                    Exception = ex,
+                    IsFatal = isFatal,
+                    LogFile = log.Destination
+                });
+            errorForm.ShowDialog(_mainForm);
+        }
+    }
+}
+
