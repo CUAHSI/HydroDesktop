378ced3bba23dd895547f96beda9b281e6324373
ericch <ericch@localhost>
2013-02-18 18:31:45 -0700

    Added custom configuration for the values downloaded per request by adding a Download Settings form. Also updated Dotspatial dlls.

---
+++ Source/HydroDesktop.Configuration/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HydroDesktop.Configuration")]
+[assembly: AssemblyDescription("HydroDesktop Configuration Settings Management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CUAHSI")]
+[assembly: AssemblyProduct("HydroDesktop.Configuration")]
+[assembly: AssemblyCopyright("Copyright © CUAHSI 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55b10f46-85ac-40bb-bc56-3d7bd32c7d5b")]
