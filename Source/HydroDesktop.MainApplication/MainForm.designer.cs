59b945aeed52e290cf3f8f9cb0d888a8e036fa7a
shieldst <shieldst@localhost>
2013-04-23 11:18:30 -0600

    Implemented a couple more aspects of the SimpleHeaderControl. Also changed the simple DockManager so that it is at least slightly usable for now.

--- Source/HydroDesktop.MainApplication/MainForm.designer.cs
+++ Source/HydroDesktop.MainApplication/MainForm.designer.cs
@@ -53,10 +53,10 @@ namespace HydroDesktop.MainApplication
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1040, 662);
+            this.ClientSize = new System.Drawing.Size(1284, 662);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "HydroDesktop";
             this.ResumeLayout(false);
 
