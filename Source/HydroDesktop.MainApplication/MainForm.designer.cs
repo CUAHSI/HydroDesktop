bc51da72a19b2e19617aba4ef1c9daae0e649073
shieldst <shieldst@localhost>
2013-04-24 11:11:47 -0600

    Changed the System.Data.SQLite (and .Linq) DLL references to use the "Managed- Only" versions. This means that the DLLs look for SQLite3 on the machine instead of SQLite.Interop.dll. Did this to get HydroDesktop.Data working on Mono. The user must have sqlite3 installed on his or her machine, so this may need to be changed further, but it's a good first step.

--- Source/HydroDesktop.MainApplication/MainForm.designer.cs
+++ Source/HydroDesktop.MainApplication/MainForm.designer.cs
@@ -53,7 +53,7 @@ namespace HydroDesktop.MainApplication
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1284, 662);
+            this.ClientSize = new System.Drawing.Size(871, 149);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
