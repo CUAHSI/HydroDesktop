63b5fc343bfccf728b37c74536cc14b42030c0e1
shieldst <shieldst@localhost>
2013-05-07 13:32:48 -0600

    Removed 64 bit sqlite3.dll because I believe it was not the right version. I retargeted HydroDesktop to 32bit because we don't have a 64 bit version of sqlite3 now. Once we get this version of sqlite to work completely, then we can figure out how to build a 64 bit version of sqlite3 and target 64 bit again.

---
+++ Source/HydroDesktop.SplashScreenManager/SplashScreen.cs
@@ -0,0 +1,60 @@
+﻿// adopted code from:
+// http://www.codeproject.com/Articles/37886/Yet-Another-Splash-Screen-in-C
+// ************************************************************************
+
+
+namespace HydroDesktop.SplashScreenManager
+{
+    /// <summary>
+    /// Defined types of messages: Success/Warning/Error.
+    /// </summary>
+    public enum TypeOfMessage
+    {
+        Success,
+        Warning,
+        Error,
+    }
+    /// <summary>
+    /// Initiate instance of SplashScreen
+    /// </summary>
+    public static class SplashScreen
+    {
+        static SplashScreenForm sf = null;
+
+        /// <summary>
+        /// Displays the splashscreen
+        /// </summary>
+        public static void ShowSplashScreen()
+        {
+            if (sf == null)
+            {
+                sf = new SplashScreenForm();
+                sf.ShowSplashScreen();
+            }
+        }
+
+        /// <summary>
+        /// Closes the SplashScreen
+        /// </summary>
+        public static void CloseSplashScreen()
+        {
+            if (sf != null)
+            {
+                sf.CloseSplashScreen();
+                sf = null;
+            }
+        }
+
+        /// <summary>
+        /// Update the status text in default color
+        /// </summary>
+        /// <param name="Text">Message</param>
+        public static void UdpateStatusText(string Text)
+        {
+            if (sf != null)
+                sf.UdpateStatusText(Text);
+
+        }
+    }
+
+}
