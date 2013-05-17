80a3e3879fa3da82ab52e75c6b02d9d15e0d3e49
shieldst <shieldst@localhost>
2013-05-17 13:22:55 -0600

    Changed the Mac Bundle Maker script slightly.

--- SupportFiles/Mac Bundle Files/HydroDestkopBundleMaker.bat
+++ SupportFiles/Mac Bundle Files/HydroDestkopBundleMaker.bat
@@ -2,17 +2,39 @@
 :: Use the resulting folder (HydroDesktop.app) on Mac OS as an application.
 :: The bundle can be used to create a .pkg with Apple's PackageMaker.app.
 
-SET SOURCE=
-SET DESTINATION=
+::You can change the SOURCE and DESTINATION if necessary, otherwise leave as is.
+
+SET SOURCE=..\..
+SET DESTINATION=..\..
 
 xcopy "%SOURCE%\Binaries\*.dll" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\"
 xcopy "%SOURCE%\Binaries\*.exe" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\"
 xcopy "%SOURCE%\Binaries\*.png" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\"
+
 xcopy "%SOURCE%\Binaries\Mono Extensions\*.dll" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Mono Extensions\"
+
 xcopy "%SOURCE%\Binaries\Application Extensions\*.dll" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Application Extensions\"
-xcopy "%SOURCE%\Binaries\Plugins\*.dll" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+
 xcopy "%SOURCE%\Binaries\Support\Mono\*.*" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Support\Mono\"
-xcopy "%SOURCE%\Binaries\Plugins\*" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+
+xcopy "%SOURCE%\Binaries\Plugins\*.dll" /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\DataAggregation\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\DataAggregation\*.dll.config" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\EPADelineation\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\EPADelineation\*.dll.config" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\ExcelExtension\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\ExportToCSV\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\ExportToCSV\*.dll.config" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\GraphView\GraphView.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\HelpTab\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\HelpTab\*.dll.config" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\ImportFromWaterML\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\MetadataFetcher\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\Search3\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\Search3\*.dll.config" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+xcopy "%SOURCE%\Binaries\Plugins\Search3\Resources\* /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\Resources\"
+xcopy "%SOURCE%\Binaries\Plugins\TableView\*.dll" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\Plugins\"
+
 xcopy "%SOURCE%\Binaries\de\*" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\de\"
 xcopy "%SOURCE%\Binaries\es\*" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\es\"
 xcopy "%SOURCE%\Binaries\fr\*" /s /y "%DESTINATION%\HydroDesktop.app\Contents\MacOS\fr\"
