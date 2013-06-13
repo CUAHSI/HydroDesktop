b7bfea70b80d7bd2767bd734b6581278906cc8f2
cuylerfris <cuylerfris@localhost>
2013-06-13 15:19:01 -0600

    Renamed "Results" to "Search Results" on the search ribbon Removed draw rectangle button from search ribbon Moved current view button to far left side of search ribbon

---
+++ Source/HydroDesktop.Interfaces/SeriesEventArgs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HydroDesktop.Interfaces
+{
+    /// <summary>
+    /// Event arguments for the SeriesChecked event
+    /// </summary>
+    public class SeriesEventArgs : EventArgs
+    {
+        private readonly int _seriesID;
+        private readonly bool _isChecked;
+
+        /// <summary>
+        /// Creates a new instance of the series EventArgs
+        /// </summary>
+        /// <param name="seriesID">Series ID</param>
+        /// <param name="isChecked">True if new staus is checked, 
+        /// false if new status is unchecked</param>
+        public SeriesEventArgs(int seriesID, bool isChecked)
+        {
+            _seriesID = seriesID;
+            _isChecked = isChecked;
+        }
+
+        /// <summary>
+        /// The checked or unchecked series ID
+        /// </summary>
+        public int SeriesID
+        {
+            get { return _seriesID;  }
+        }
+
+        /// <summary>
+        /// The check state (checked or unchecked)
+        /// </summary>
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+        }
+    }
+
+    #region Delegates
+    /// <summary>
+    /// Series checked event handler
+    /// </summary>
+    /// <param name="sender">sender object</param>
+    /// <param name="e">information about checked or unchecked series</param>
+    public delegate void SeriesEventHandler(object sender, SeriesEventArgs e);
+    #endregion
+        
+}
