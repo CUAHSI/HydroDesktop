44792c01667a7ccfd571cacb2aa8f0fa21e11d9e
ericch <ericch@localhost>
2013-06-17 18:49:23 -0600

    including a config file

---
+++ Documentation/OnlineHelp/source/extensions/EditView/Introduction.rst
@@ -0,0 +1,7 @@
+.. index:: introduction
+
+Introduction
+=====================================================
+  
+The Edit View is a HydroDesktop extension that provides functionality for editing time series data that have been downloaded and stored in the HydroDesktop data repository database.  The Edit View enables you to perform a number of data editing functions that can be very useful in doing simple data quality assurance and quality control as well as in preparing time series data that may have some anomalous data values for input to an analysis or model.
+
---
+++ Source/EPADelineation/Progress.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EPADelineation
+{
+    public partial class FmProgress : Form
+    {
+        /// <summary>
+        /// True if the process is still working
+        /// </summary>
+        public bool _isworking = false;
+
+        /// <summary>
+        /// Creates a new instance of the progress dialog form
+        /// </summary>
+        public FmProgress()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Updates text in the progress bar dialog
+        /// </summary>
+        /// <param name="startpt">the comid of the outlet point</param>
+        public void updateText(object[] startpt)
+        {
+            _isworking = true;
+
+            try
+            {
+                string comid = startpt[0] as string;
+                string measure = startpt[1] as string;
+
+                this.lblmessage2.Text = "Sending Request to EPA Delineation Web Services \n for   COMID = " + comid + " \n and MEASURE = " + measure + "...";
+                this.lblmessage1.Visible = false;
+                this.lblmessage2.Visible = true;
+                this.lblmessage3.Visible = false;
+
+                //Update the Label Text
+                Application.DoEvents();
+
+                _isworking = false;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.closeForm(); 
+
+                _isworking = false;
+            }
+        }
+        /// <summary>
+        /// updates progress status text in the progress form
+        /// </summary>
+        public void updateText()
+        {
+            _isworking = true;
+
+            try
+            {
+                this.lblmessage3.Visible = true;
+                this.lblmessage1.Visible = false;
+                this.lblmessage2.Visible = false;
+
+                Application.DoEvents();
+
+                _isworking = false;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.closeForm(); 
+                _isworking = false;
+            }
+        }
+
+        /// <summary>
+        /// Closes the progress dialog form
+        /// </summary>
+        public void closeForm()
+        {
+            _isworking = true;
+            try
+            {
+                this.Close();
+                _isworking = false;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.closeForm(); 
+
+                _isworking = false;
+            }
+        }
+
+    }
+}
