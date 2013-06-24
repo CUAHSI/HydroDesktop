2b55feba6d89c5699b16664246010518d71ce6c7
ericch <ericch@localhost>
2013-06-24 14:09:38 -0600

    adjustments to Web Services Dialog

--- Source/Search3/Settings/UI/WebServicesUserControl.Designer.cs
+++ Source/Search3/Settings/UI/WebServicesUserControl.Designer.cs
@@ -40,6 +40,7 @@
             this.gridViewWebServices.Name = "gridViewWebServices";
             this.gridViewWebServices.Size = new System.Drawing.Size(376, 436);
             this.gridViewWebServices.TabIndex = 40;
+            this.gridViewWebServices.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridViewWebServices_CellContentClick);
             // 
             // WebServicesUserControl
             // 
