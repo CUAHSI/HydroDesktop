c0c40afd0bfa54f0c74258c6394083ed87d1cb5f
jacobean <jacobean@localhost>
2013-06-25 11:17:21 -0600

    Gradient removed on legend points.

---
+++ Source/Search3/Settings/UI/WebServicesDialog.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Search3.Settings.UI
+{
+    partial class WebServicesDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.paButtons = new System.Windows.Forms.Panel();
+            this.bntAddLocalDataSource = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSelectNone = new System.Windows.Forms.Button();
+            this.btnSelectAll = new System.Windows.Forms.Button();
+            this.rbLocalMetadataCache = new System.Windows.Forms.RadioButton();
+            this.rbHisCentral = new System.Windows.Forms.RadioButton();
+            this.gbTypeOfCatalog = new System.Windows.Forms.GroupBox();
+            this.btnManageDataSources = new System.Windows.Forms.Button();
+            this.webServicesUserControl1 = new Search3.Settings.UI.WebServicesUserControl();
+            this.paButtons.SuspendLayout();
+            this.gbTypeOfCatalog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(338, 468);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(257, 468);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // paButtons
+            // 
+            this.paButtons.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paButtons.Controls.Add(this.btnManageDataSources);
+            this.paButtons.Controls.Add(this.bntAddLocalDataSource);
+            this.paButtons.Controls.Add(this.btnRefresh);
+            this.paButtons.Controls.Add(this.btnSelectNone);
+            this.paButtons.Controls.Add(this.btnSelectAll);
+            this.paButtons.Location = new System.Drawing.Point(0, 66);
+            this.paButtons.Name = "paButtons";
+            this.paButtons.Size = new System.Drawing.Size(425, 68);
+            this.paButtons.TabIndex = 45;
+            // 
+            // bntAddLocalDataSource
+            // 
+            this.bntAddLocalDataSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bntAddLocalDataSource.Location = new System.Drawing.Point(269, 8);
+            this.bntAddLocalDataSource.Name = "bntAddLocalDataSource";
+            this.bntAddLocalDataSource.Size = new System.Drawing.Size(144, 23);
+            this.bntAddLocalDataSource.TabIndex = 44;
+            this.bntAddLocalDataSource.Text = "Add Data Source...";
+            this.bntAddLocalDataSource.UseVisualStyleBackColor = true;
+            this.bntAddLocalDataSource.Click += new System.EventHandler(this.bntAddLocalDataSource_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(13, 8);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 41;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSelectNone
+            // 
+            this.btnSelectNone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectNone.Location = new System.Drawing.Point(95, 37);
+            this.btnSelectNone.Name = "btnSelectNone";
+            this.btnSelectNone.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectNone.TabIndex = 43;
+            this.btnSelectNone.Text = "Select None";
+            this.btnSelectNone.UseVisualStyleBackColor = true;
+            this.btnSelectNone.Click += new System.EventHandler(this.btnSelectNone_Click);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectAll.Location = new System.Drawing.Point(14, 37);
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectAll.TabIndex = 42;
+            this.btnSelectAll.Text = "Select All";
+            this.btnSelectAll.UseVisualStyleBackColor = true;
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // rbLocalMetadataCache
+            // 
+            this.rbLocalMetadataCache.AutoSize = true;
+            this.rbLocalMetadataCache.Location = new System.Drawing.Point(113, 19);
+            this.rbLocalMetadataCache.Name = "rbLocalMetadataCache";
+            this.rbLocalMetadataCache.Size = new System.Drawing.Size(114, 17);
+            this.rbLocalMetadataCache.TabIndex = 47;
+            this.rbLocalMetadataCache.TabStop = true;
+            this.rbLocalMetadataCache.Text = "Local Data Source";
+            this.rbLocalMetadataCache.UseVisualStyleBackColor = true;
+            // 
+            // rbHisCentral
+            // 
+            this.rbHisCentral.AutoSize = true;
+            this.rbHisCentral.Location = new System.Drawing.Point(9, 19);
+            this.rbHisCentral.Name = "rbHisCentral";
+            this.rbHisCentral.Size = new System.Drawing.Size(79, 17);
+            this.rbHisCentral.TabIndex = 46;
+            this.rbHisCentral.TabStop = true;
+            this.rbHisCentral.Text = "HIS Central";
+            this.rbHisCentral.UseVisualStyleBackColor = true;
+            // 
+            // gbTypeOfCatalog
+            // 
+            this.gbTypeOfCatalog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbTypeOfCatalog.Controls.Add(this.rbLocalMetadataCache);
+            this.gbTypeOfCatalog.Controls.Add(this.rbHisCentral);
+            this.gbTypeOfCatalog.Location = new System.Drawing.Point(0, 9);
+            this.gbTypeOfCatalog.Name = "gbTypeOfCatalog";
+            this.gbTypeOfCatalog.Size = new System.Drawing.Size(425, 53);
+            this.gbTypeOfCatalog.TabIndex = 49;
+            this.gbTypeOfCatalog.TabStop = false;
+            this.gbTypeOfCatalog.Text = "Select type of catalog";
+            // 
+            // btnManageDataSources
+            // 
+            this.btnManageDataSources.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnManageDataSources.Location = new System.Drawing.Point(269, 37);
+            this.btnManageDataSources.Name = "btnManageDataSources";
+            this.btnManageDataSources.Size = new System.Drawing.Size(144, 23);
+            this.btnManageDataSources.TabIndex = 45;
+            this.btnManageDataSources.Text = "Manage Data Sources...";
+            this.btnManageDataSources.UseVisualStyleBackColor = true;
+            this.btnManageDataSources.Click += new System.EventHandler(this.btnManageDataSources_Click);
+            // 
+            // webServicesUserControl1
+            // 
+            this.webServicesUserControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webServicesUserControl1.Location = new System.Drawing.Point(0, 140);
+            this.webServicesUserControl1.Name = "webServicesUserControl1";
+            this.webServicesUserControl1.Size = new System.Drawing.Size(425, 301);
+            this.webServicesUserControl1.TabIndex = 0;
+            // 
+            // WebServicesDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 505);
+            this.Controls.Add(this.gbTypeOfCatalog);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.paButtons);
+            this.Controls.Add(this.webServicesUserControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WebServicesDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Sources";
+            this.paButtons.ResumeLayout(false);
+            this.gbTypeOfCatalog.ResumeLayout(false);
+            this.gbTypeOfCatalog.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WebServicesUserControl webServicesUserControl1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Panel paButtons;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSelectNone;
+        private System.Windows.Forms.Button btnSelectAll;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.RadioButton rbLocalMetadataCache;
+        private System.Windows.Forms.RadioButton rbHisCentral;
+        private System.Windows.Forms.GroupBox gbTypeOfCatalog;
+        private System.Windows.Forms.Button bntAddLocalDataSource;
+        private System.Windows.Forms.Button btnManageDataSources;
+    }
+}
---
+++ Source/Search3/Settings/UI/WebServicesUserControl.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Data;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Search3.Settings.UI
+{
+    public partial class WebServicesUserControl : UserControl
+    {
+        #region Fields
+
+        private WebServicesSettings _webServicesSettings;
+        private CatalogSettings _catalogSettings;
+
+        #endregion
+
+        #region Constructors
+
+        public WebServicesUserControl()
+        {
+            InitializeComponent();
+
+            gridViewWebServices.CellContentClick += gridViewWebServices_OpenUrl;
+            gridViewWebServices.CellValueChanged += gridViewWebServices_AfterCheck;  
+        }
+
+     
+
+        #endregion
+
+        #region Private methods
+
+        void gridViewWebServices_AfterCheck(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1) return;
+            
+            var webNode = (WebServiceNode)gridViewWebServices.Rows[e.RowIndex].Tag;
+            if (webNode == null) return;
+
+            DataGridViewCheckBoxCell cell = (DataGridViewCheckBoxCell)gridViewWebServices.Rows[e.RowIndex].Cells[e.ColumnIndex];
+           if((bool)cell.Value == true)
+           {
+               webNode.Checked = true;
+           }
+           else 
+           {
+               webNode.Checked = false;
+           }
+          
+           // webNode.Checked = gridViewWebServices.Rows[e.RowIndex].Cells[e.ColumnIndex].c
+
+           // var webNode = (WebServiceNode)e.Node.Tag;
+           // webNode.Checked = e.Node.Checked;
+        }
+
+        private void gridViewWebServices_OpenUrl(object sender, DataGridViewCellEventArgs e)
+        {
+             gridViewWebServices.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            if (gridViewWebServices.Columns[e.ColumnIndex] is DataGridViewLinkColumn)
+            {
+                //do your stuff here
+                string link = gridViewWebServices[e.ColumnIndex, e.RowIndex].Value.ToString();
+
+                try
+                {
+                    System.Diagnostics.Process.Start(link);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Can't open url. Exception:" + ex.Message);
+                }
+            }
+            
+           // if (e.ColumnIndex != 0) return;
+
+            
+            /*
+            var node = e.Node;
+            var nodeInfo = node.Tag as WebServiceNode;
+            if (nodeInfo == null || 
+                nodeInfo.DescriptionUrl == null)
+            {
+                MessageBox.Show("The node [" + node.Text + "] don't have any ServiceDescriptionURL attribute.");
+                return;
+            }
+            */  
+        }
+
+        private void RefreshWebServicesGridView(IEnumerable<WebServiceNode> webServiceNodeCollection)
+        {
+            gridViewWebServices.SuspendLayout();
+            try
+            {
+                gridViewWebServices.Columns.Clear();
+               
+                DataGridViewCheckBoxColumn colCB = new DataGridViewCheckBoxColumn();
+                colCB.Name = "chkcol";
+                colCB.HeaderText = "";
+                colCB.Width = 20;
+                colCB.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                gridViewWebServices.Columns.Add(colCB);
+
+                DataGridViewTextBoxColumn colTB = new DataGridViewTextBoxColumn();
+                colTB.Name = "Organization";
+                colTB.HeaderText = "Organization";
+                gridViewWebServices.Columns.Add(colTB);
+
+                DataGridViewTextBoxColumn colTB2 = new DataGridViewTextBoxColumn();
+                colTB2.Name = "Service Code";
+                colTB2.HeaderText = "Service Code";
+                gridViewWebServices.Columns.Add(colTB2);
+
+                DataGridViewLinkColumn dgvlc = new DataGridViewLinkColumn();
+                dgvlc.HeaderText = "Link";
+                dgvlc.Width = 260;
+                gridViewWebServices.Columns.Add(dgvlc);
+
+              
+
+                gridViewWebServices.AllowUserToAddRows = true;
+                foreach (var webNode in webServiceNodeCollection)
+                {
+                   
+                    DataGridViewRow row = (DataGridViewRow)gridViewWebServices.Rows[0].Clone();
+                    row.Cells[0].Value = webNode.Checked;
+                    row.Cells[1].Value = webNode.Title;
+                    row.Cells[2].Value = webNode.ServiceCode;
+                    row.Cells[3].Value = webNode.DescriptionUrl;
+                    row.Tag = webNode;
+                    gridViewWebServices.Rows.Add(row);
+                }
+                gridViewWebServices.AllowUserToAddRows = false;
+
+              //  gridViewWebServices.Rows[0].Cells["Web Service"].Tag;
+
+                //gridViewWebServices.Data
+                /*
+                var Rows = gridViewWebServices.Rows;
+                var clrBule = Color.FromKnownColor(KnownColor.Blue);
+                var prototype = gridViewWebServices.Font;
+                var font = new Font(prototype, FontStyle.Underline);
+
+                foreach (var webNode in webServiceNodeCollection)
+                {
+              
+                    var node = new DataRow
+                    {
+                        ForeColor = clrBule,
+                        NodeFont = font,
+                        Text = webNode.Title,
+                        Name = webNode.ServiceID.ToString(CultureInfo.InvariantCulture),
+                        Checked = webNode.Checked,
+                        Tag = webNode
+                    };
+                    parentRows.Add(node);
+                }
+                gridViewWebServices.Sort();*/
+            }
+            finally
+            {
+                gridViewWebServices.ResumeLayout();
+            }
+        }
+
+       
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Refresh all web services.
+        /// </summary>
+        public void RefreshWebServices()
+        {
+            if (_webServicesSettings == null)
+            {
+                return;
+            }
+
+            _webServicesSettings.RefreshWebServices(_catalogSettings);
+            RefreshWebServicesGridView(_webServicesSettings.WebServices);
+        }
+
+
+        /// <summary>
+        /// Check all web services.
+        /// </summary>
+        /// <param name="check">Check or uncheck all web services.</param>
+        public void CheckAllWebServices(bool check)
+        {
+            if (gridViewWebServices.Rows.Count <= 0) return;
+           
+            foreach (DataGridViewRow row in gridViewWebServices.Rows)
+            {
+                DataGridViewCheckBoxCell chk = (DataGridViewCheckBoxCell)row.Cells[0];
+                chk.Value = check;
+            }
+            
+        }
+
+        /// <summary>
+        /// Set settings into control.
+        /// </summary>
+        /// <param name="webServicesSettings">WebServices settings to set.</param>
+        /// <param name="catalogSettings">Catalog settings </param>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="webServicesSettings"/> is null.</exception>
+        public void SetSettings(WebServicesSettings webServicesSettings, CatalogSettings catalogSettings)
+        {
+            if (webServicesSettings == null) throw new ArgumentNullException("webServicesSettings");
+
+            _catalogSettings = catalogSettings;
+            _webServicesSettings = webServicesSettings;
+            RefreshWebServicesGridView(webServicesSettings.WebServices);
+        }
+
+        #endregion
+    }
+}
