f205659da8461fbc363a5aa608ef5c3bd321b1d1
jacobean <jacobean@localhost>
2013-07-26 15:23:08 -0600

    Made some changes so that the search and shale extensions won't interfere.

--- Source/DataDownload/DataDownload/DataDownloadPlugin.cs
+++ Source/DataDownload/DataDownload/DataDownloadPlugin.cs
@@ -223,7 +223,7 @@ namespace HydroDesktop.DataDownload
         private void AppOnExtensionsActivated(object sender, EventArgs eventArgs)
         {
             // Add download button into search tab
-            if (App.GetExtension("Search3") != null)
+            if (App.GetExtension("Search3") != null && App.GetExtension("ShaleDataNetwork") == null)
             {
                 App.HeaderControl.Add(_btnSearchResults = new SimpleActionItem("Show Attribute Table", ShowSearchResults_Click) { RootKey = SharedConstants.SearchRootkey, GroupCaption = Msg.Results, SmallImage = Resources.table_16x16, Enabled = false, ToggleGroupKey = MessageStrings.Search_Results_Tools_Group });
                 _btnSearchResults.PropertyChanged += btnSearchResults_enabled;
@@ -250,10 +250,14 @@ namespace HydroDesktop.DataDownload
                 string shale = "Shale Data Network";
 
                 App.HeaderControl.Add(_btnSearchResults = new SimpleActionItem("Show Attribute Table", ShowSearchResults_Click) { RootKey = shale, GroupCaption = Msg.Results, SmallImage = Resources.table_16x16, Enabled = false, ToggleGroupKey = MessageStrings.Search_Results_Tools_Group });
-                App.HeaderControl.Add(_btnShowPopups = new SimpleActionItem("Show Map Popups", ShowPopups_Click) { RootKey = shale, GroupCaption = Msg.Results, SmallImage = Resources.popup_16x16, ToggleGroupKey = Msg.Download_Tools_Group, Enabled = false });
-               
-                _btnShowPopups.Toggling += ShowPopups_Click;
-                _btnShowPopups.Enabled = false;
+                _btnSearchResults.PropertyChanged += btnSearchResults_enabled;
+
+                if (!DotSpatial.Mono.Mono.IsRunningOnMono())
+                {
+                    App.HeaderControl.Add(_btnShowPopups = new SimpleActionItem("Show Map Popups", ShowPopups_Click) { RootKey = shale, GroupCaption = Msg.Results, SmallImage = Resources.popup_16x16, ToggleGroupKey = Msg.Download_Tools_Group, Enabled = false });
+                    _btnShowPopups.Toggling += ShowPopups_Click;
+                    _btnShowPopups.Enabled = false;
+                }
                 _showPopups = false;
 
                 App.HeaderControl.Add(_btnSearchOptions = new SimpleActionItem("Download Settings", Options_Click) { RootKey = shale, GroupCaption = Msg.Results, SmallImage = Resources.settings_16, ToolTipText = Msg.DownloadSettings, Enabled = true });
