7854fd707b6cc7dde758c1003be43105b4e94a70
jacobean <jacobean@localhost>
2013-09-30 17:24:02 -0600

    Changed the DotSpatial.Plugins.Ribbon back to the old one which uses DevExpress12.

---
+++ Source/HydroModeler 2.0/ConfigurationEditor/AdaptedArguments.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using OpenMI.Standard2;
+
+namespace Oatc.OpenMI.Gui.ConfigurationEditor
+{
+    public partial class AdaptedArguments : Form
+    {
+        DataTable _table;
+        IList<IArgument> _arguments;
+        DataGridViewCell _cellInEdit = null;
+
+        public AdaptedArguments()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialise(IList<IArgument> args)
+        {
+            _arguments = args;
+
+            _table = new DataTable();
+
+            DataColumn column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "ID";
+            column.ReadOnly = true;
+            column.Unique = true;
+
+            _table.Columns.Add(column);
+
+            column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "Caption";
+            column.ReadOnly = true;
+            column.Unique = false;
+
+            _table.Columns.Add(column);
+
+            column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "Value";
+            column.ReadOnly = false;
+            column.Unique = false;
+
+            _table.Columns.Add(column);
+
+            column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "Type";
+            column.ReadOnly = true;
+            column.Unique = false;
+
+            _table.Columns.Add(column);
+
+            column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "Default";
+            column.ReadOnly = true;
+            column.Unique = false;
+
+            _table.Columns.Add(column);
+
+            column = new DataColumn();
+
+            column.DataType = typeof(String);
+            column.ColumnName = "Description";
+            column.ReadOnly = true;
+            column.Unique = false;
+
+            _table.Columns.Add(column);
+            DataRow row;
+
+            foreach (IArgument arg in args)
+            {
+                row = _table.NewRow();
+                row["ID"] = arg.Id;
+                row["Caption"] = arg.Caption;
+                row["Value"] = arg.Value;
+                row["Type"] = arg.ValueType.ToString();
+                row["Default"] = arg.DefaultValue.ToString();
+                row["Description"] = arg.Description;
+                _table.Rows.Add(row);
+            }
+
+            dataGridView1.DataSource = _table;
+
+            dataGridView1.CellBeginEdit += new DataGridViewCellCancelEventHandler(CellBeginEdit);
+            dataGridView1.CellEndEdit += new DataGridViewCellEventHandler(CellEndEdit);
+
+            dataGridView1.Columns[0].Visible = false; // hide Id's
+
+            for (int nR = 0; nR < dataGridView1.Rows.Count; ++nR)
+                for (int nC = 0; nC < dataGridView1.Rows[nR].Cells.Count; ++nC)
+                {
+                    if (dataGridView1.Rows[nR].Cells[nC].ReadOnly)
+                        dataGridView1.Rows[nR].Cells[nC].Style.BackColor = Color.LightGray;
+                }
+        }
+
+        public void UpdateArgumentsFromForm()
+        {
+            for (int n = 0; n < _arguments.Count; ++n)
+                _arguments[n].ValueAsString = (string)_table.Rows[n][1];
+        }
+
+        void CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            Debug.Assert(_cellInEdit == null);
+            _cellInEdit = ((DataGridView)sender).CurrentCell;
+        }
+
+        void CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            EditValue();
+        }
+
+        void EditValue()
+        {
+            Debug.Assert(_cellInEdit != null);
+
+            if (!_cellInEdit.ReadOnly)
+                UpdateArgFromCell();
+
+            _cellInEdit = null;
+        }
+
+        void UpdateArgFromCell()
+        {
+            string value = (string)_cellInEdit.Value;
+            string oldValue = _arguments[_cellInEdit.RowIndex].ValueAsString;
+
+            try
+            {
+                _arguments[_cellInEdit.RowIndex].ValueAsString = value;
+            }
+            catch (Exception e)
+            {
+                _arguments[_cellInEdit.RowIndex].Value = oldValue;
+                _cellInEdit.Value = oldValue;
+
+                string s = string.Format(
+                    "Invalid Value \"{0}\"\r\n"
+                    + "Value should be of type {1}",
+                        value,
+                        _arguments[_cellInEdit.RowIndex].ValueType.ToString());
+
+                MessageBox.Show(s, e.Message, 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void OnClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_cellInEdit != null)
+            {
+                dataGridView1.NotifyCurrentCellDirty(true);
+
+                EditValue();
+            }
+        }
+    }
+}
---
+++ Source/HydroShare/Lib/hydrosharedownload.py
@@ -0,0 +1,79 @@
+import urllib2
+import os
+import json
+import zipfile
+
+class HydroshareDownloader():
+    #base_url points to the Export.php script that packages data files for download.
+    base_url="http://dev.hydroshare.org/export.php?file=/home/hydroadmin/hydroshare/sites/default/files/{0}.zip"
+    	
+    #The url from which a list of available resources can be found
+    list_url="http://dev.hydroshare.org/?q=my_services/node.json"
+
+    #Local filepath where the downloaded files will be saved
+    file_path=""
+
+    def downloadFile(self, resource):
+        '''Downloads a file with given name from HydroShare and saves it to the specified location.'''
+    
+        #Remove spaces from Resource name
+        resource = resource.replace(" ", "")
+        	
+        #Combine the Save folder with the resource name
+        save_loc = os.path.join(self.file_path, resource + ".zip")
+        	
+        #Download and save the file
+        file = urllib2.urlopen(self.base_url.format(resource))
+        with open(save_loc, "wb") as code:
+            code.write(file.read())
+        
+        self.unzipData(save_loc, resource)
+
+    def retrieveList(self):
+        '''Download list of files as JSON string and filters it to return list of available files.'''
+        data = urllib2.urlopen(self.list_url)
+
+        all_files = json.load(data)
+        filtered_files = []
+        	
+        for file in all_files:
+            if (file["type"] == "hydroshare_time_series" 
+            or file["type"] == "hydroshare_geoanalytics"):
+                filtered_files.append(file["title"])
+        
+        return filtered_files
+
+    def unzipData(self, saved_loc, resource):
+        zipped = zipfile.ZipFile(saved_loc)
+        temp_path = os.path.join(self.file_path, "result")
+        save_path = os.path.join(self.file_path, resource)
+        try:
+            os.makedirs(temp_path)
+            os.makedirs(save_path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+        zipped.extractall(temp_path)
+        data_zipped = zipfile.ZipFile(os.path.join(temp_path, data, )
+
+        
+    def test(self):
+        '''Test this script.'''
+        self.test_retrieveList()
+        self.test_downloadFile()
+
+    def test_downloadFile(self):
+        '''Test method that saves a test file to same folder as script'''
+        self.downloadFile("EdgarRanchComparisonTest1")
+
+    def test_retrieveList(self):
+        '''Test method that retrieves and prints the list of files from HydroShare'''
+        list = self.retrieveList()
+        for file in list:
+            print(file)
+
+#Run test when script is run from the command line
+if __name__ == '__main__':
+    tester = HydroshareDownloader()
+    tester.test()
