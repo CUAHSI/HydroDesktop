3c9a220ab2dc91cc6898c4c95d7dff9beb8f4b15
cuylerfris <cuylerfris@localhost>
2013-10-28 16:38:17 -0600

    Updated HydroShare ribbon icons

---
+++ Source/MetadataFetcher/Classes/Main.cs
@@ -0,0 +1,51 @@
+﻿#region Namespaces
+
+using System.Windows.Forms;
+using DotSpatial.Controls;
+using HydroDesktop.Interfaces.PluginContracts;
+using HydroDesktop.MetadataFetcher.Forms;
+
+#endregion
+
+namespace HydroDesktop.MetadataFetcher
+{
+    class Main : Extension, IMetadataFetcherPlugin
+	{
+		#region IExtension Members
+		
+		public override void Deactivate ()
+		{
+            App.HeaderControl.RemoveAll();
+
+			//necessary in plugin deactivation
+			base.Deactivate ();
+		}
+
+        #endregion
+
+        #region Implementation of IMetadataFetcherPlugin
+
+        public void AddServices()
+        {
+            using (var form = new AddServicesForm())
+            {
+                var dialogResult = form.ShowDialog();
+                if (dialogResult == DialogResult.OK)
+                {
+                    DownloadMetadata();
+                }   
+            }
+        }
+
+        public void DownloadMetadata()
+        {
+            using (var form = new MainForm())
+            {
+                form.RefreshServiceList();
+                form.ShowDialog();
+            }
+        }
+
+        #endregion
+	}
+}
