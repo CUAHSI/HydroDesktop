50ce729e9ee292e736dbf975971974be322f1d28
gespinoza <gespinoza@localhost>
2013-11-18 10:08:23 -0700

    Adding Aggregation Plugin

---
+++ Source/Aggregation_Plugin/Parameters_form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DotSpatial.Controls;
+using DotSpatial.Controls.Header;
+
+namespace Aggregation_Plugin
+{
+    public partial class Parameters_form : Form
+    {
+        public Parameters_form()
+        {
+            InitializeComponent();
+
+            //var allLayers = Map.GetPolygonLayers();
+            //foreach (layer in allLayers)
+                //Polygon_cbx.Items.Add(layer);
+
+            //AppManager _mapArgs;
+
+            //var allLayers = ((Map)_mapArgs.Map).GetAllLayers();
+            //Polygon_cbx.Items.Add(allLayers);
+            //foreach (layer in allLayers)
+                //Polygon_cbx.Items.Add(layer);
+
+            
+
+        }
+
+    }
+}
