b761c74904888ed2148d572c5e28a833cf29fc41
jacobean <jacobean@localhost>
2013-11-18 18:03:00 -0700

    Aggregation plugin now popultates some of the dropdowns.

--- Source/Aggregation_Plugin/Parameters_form.Designer.cs
+++ Source/Aggregation_Plugin/Parameters_form.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.OK_click = new System.Windows.Forms.Button();
             this.Cancel_click = new System.Windows.Forms.Button();
-            this.Polygon_cbx = new System.Windows.Forms.ComboBox();
-            this.Points_cbx = new System.Windows.Forms.ComboBox();
+            this.PolygonLayerList = new System.Windows.Forms.ComboBox();
+            this.SiteList = new System.Windows.Forms.ComboBox();
             this.Polygon_Label = new System.Windows.Forms.Label();
             this.Point_Label = new System.Windows.Forms.Label();
             this.Variable_Label = new System.Windows.Forms.Label();
             this.Output_txt = new System.Windows.Forms.TextBox();
-            this.Variable_cbx = new System.Windows.Forms.ComboBox();
+            this.VariableList = new System.Windows.Forms.ComboBox();
             this.Output_Label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // OK_click
             // 
-            this.OK_click.Location = new System.Drawing.Point(141, 116);
+            this.OK_click.Location = new System.Drawing.Point(41, 116);
             this.OK_click.Name = "OK_click";
             this.OK_click.Size = new System.Drawing.Size(75, 23);
             this.OK_click.TabIndex = 0;
@@ -51,98 +51,99 @@
             // 
             // Cancel_click
             // 
-            this.Cancel_click.Location = new System.Drawing.Point(60, 116);
+            this.Cancel_click.Location = new System.Drawing.Point(122, 116);
             this.Cancel_click.Name = "Cancel_click";
             this.Cancel_click.Size = new System.Drawing.Size(75, 23);
             this.Cancel_click.TabIndex = 1;
             this.Cancel_click.Text = "Cancel";
             this.Cancel_click.UseVisualStyleBackColor = true;
             // 
-            // Polygon_cbx
+            // PolygonLayerList
             // 
-            this.Polygon_cbx.FormattingEnabled = true;
-            this.Polygon_cbx.Location = new System.Drawing.Point(95, 9);
-            this.Polygon_cbx.Name = "Polygon_cbx";
-            this.Polygon_cbx.Size = new System.Drawing.Size(121, 21);
-            this.Polygon_cbx.TabIndex = 2;
+            this.PolygonLayerList.FormattingEnabled = true;
+            this.PolygonLayerList.Location = new System.Drawing.Point(82, 9);
+            this.PolygonLayerList.Name = "PolygonLayerList";
+            this.PolygonLayerList.Size = new System.Drawing.Size(121, 21);
+            this.PolygonLayerList.TabIndex = 2;
             // 
-            // Points_cbx
+            // SiteList
             // 
-            this.Points_cbx.FormattingEnabled = true;
-            this.Points_cbx.Location = new System.Drawing.Point(95, 36);
-            this.Points_cbx.Name = "Points_cbx";
-            this.Points_cbx.Size = new System.Drawing.Size(121, 21);
-            this.Points_cbx.TabIndex = 3;
+            this.SiteList.FormattingEnabled = true;
+            this.SiteList.Location = new System.Drawing.Point(82, 36);
+            this.SiteList.Name = "SiteList";
+            this.SiteList.Size = new System.Drawing.Size(121, 21);
+            this.SiteList.TabIndex = 3;
             // 
             // Polygon_Label
             // 
             this.Polygon_Label.AutoSize = true;
-            this.Polygon_Label.Location = new System.Drawing.Point(12, 9);
+            this.Polygon_Label.Location = new System.Drawing.Point(1, 12);
             this.Polygon_Label.Name = "Polygon_Label";
             this.Polygon_Label.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Polygon_Label.Size = new System.Drawing.Size(77, 13);
+            this.Polygon_Label.Size = new System.Drawing.Size(74, 13);
             this.Polygon_Label.TabIndex = 4;
-            this.Polygon_Label.Text = "Select polygon";
+            this.Polygon_Label.Text = "Polygon Layer";
             // 
             // Point_Label
             // 
             this.Point_Label.AutoSize = true;
-            this.Point_Label.Location = new System.Drawing.Point(2, 36);
+            this.Point_Label.Location = new System.Drawing.Point(50, 39);
             this.Point_Label.Name = "Point_Label";
             this.Point_Label.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Point_Label.Size = new System.Drawing.Size(87, 13);
+            this.Point_Label.Size = new System.Drawing.Size(25, 13);
             this.Point_Label.TabIndex = 5;
-            this.Point_Label.Text = "Select point data";
+            this.Point_Label.Text = "Site";
             // 
             // Variable_Label
             // 
             this.Variable_Label.AutoSize = true;
-            this.Variable_Label.Location = new System.Drawing.Point(12, 63);
+            this.Variable_Label.Location = new System.Drawing.Point(30, 66);
             this.Variable_Label.Name = "Variable_Label";
             this.Variable_Label.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Variable_Label.Size = new System.Drawing.Size(77, 13);
+            this.Variable_Label.Size = new System.Drawing.Size(45, 13);
             this.Variable_Label.TabIndex = 6;
-            this.Variable_Label.Text = "Select variable";
+            this.Variable_Label.Text = "Variable";
             // 
             // Output_txt
             // 
-            this.Output_txt.Location = new System.Drawing.Point(95, 90);
+            this.Output_txt.Location = new System.Drawing.Point(82, 89);
             this.Output_txt.Name = "Output_txt";
             this.Output_txt.Size = new System.Drawing.Size(121, 20);
             this.Output_txt.TabIndex = 7;
             // 
-            // Variable_cbx
+            // VariableList
             // 
-            this.Variable_cbx.FormattingEnabled = true;
-            this.Variable_cbx.Location = new System.Drawing.Point(95, 63);
-            this.Variable_cbx.Name = "Variable_cbx";
-            this.Variable_cbx.Size = new System.Drawing.Size(121, 21);
-            this.Variable_cbx.TabIndex = 8;
+            this.VariableList.FormattingEnabled = true;
+            this.VariableList.Location = new System.Drawing.Point(82, 63);
+            this.VariableList.Name = "VariableList";
+            this.VariableList.Size = new System.Drawing.Size(121, 21);
+            this.VariableList.TabIndex = 8;
             // 
             // Output_Label
             // 
             this.Output_Label.AutoSize = true;
-            this.Output_Label.Location = new System.Drawing.Point(21, 90);
+            this.Output_Label.Location = new System.Drawing.Point(5, 89);
             this.Output_Label.Name = "Output_Label";
             this.Output_Label.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Output_Label.Size = new System.Drawing.Size(68, 13);
+            this.Output_Label.Size = new System.Drawing.Size(70, 13);
             this.Output_Label.TabIndex = 9;
-            this.Output_Label.Text = "Output name";
+            this.Output_Label.Text = "Output Name";
             // 
             // Parameters_form
             // 
+            this.AcceptButton = this.OK_click;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(226, 147);
+            this.ClientSize = new System.Drawing.Size(217, 147);
             this.Controls.Add(this.Output_Label);
-            this.Controls.Add(this.Variable_cbx);
+            this.Controls.Add(this.VariableList);
             this.Controls.Add(this.Output_txt);
             this.Controls.Add(this.Variable_Label);
             this.Controls.Add(this.Point_Label);
             this.Controls.Add(this.Polygon_Label);
-            this.Controls.Add(this.Points_cbx);
-            this.Controls.Add(this.Polygon_cbx);
+            this.Controls.Add(this.SiteList);
+            this.Controls.Add(this.PolygonLayerList);
             this.Controls.Add(this.Cancel_click);
             this.Controls.Add(this.OK_click);
             this.Name = "Parameters_form";
@@ -156,13 +157,13 @@
 
         private System.Windows.Forms.Button OK_click;
         private System.Windows.Forms.Button Cancel_click;
-        private System.Windows.Forms.ComboBox Polygon_cbx;
-        private System.Windows.Forms.ComboBox Points_cbx;
+        private System.Windows.Forms.ComboBox PolygonLayerList;
+        private System.Windows.Forms.ComboBox SiteList;
         private System.Windows.Forms.Label Polygon_Label;
         private System.Windows.Forms.Label Point_Label;
         private System.Windows.Forms.Label Variable_Label;
         private System.Windows.Forms.TextBox Output_txt;
-        private System.Windows.Forms.ComboBox Variable_cbx;
+        private System.Windows.Forms.ComboBox VariableList;
         private System.Windows.Forms.Label Output_Label;
     }
 }
--- Source/Aggregation_Plugin/Parameters_form.cs
+++ Source/Aggregation_Plugin/Parameters_form.cs
@@ -8,28 +8,95 @@ using System.Text;
 using System.Windows.Forms;
 using DotSpatial.Controls;
 using DotSpatial.Controls.Header;
+using System.Windows.Controls;
+using DotSpatial.Data;
+using DotSpatial.Topology;
 
 namespace Aggregation_Plugin
 {
     public partial class Parameters_form : Form
     {
-        public Parameters_form()
+        AppManager App;
+        FeatureSet polygons = new FeatureSet(FeatureType.Polygon);
+
+        public Parameters_form(AppManager App)
         {
             InitializeComponent();
+            this.App = App;
+            populatePolygonLayerDropdown();
+            populateSites();
+            App.Map.MapFrame.SelectionChanged += SelectionChanged;
+            PolygonLayerList.SelectedValueChanged += SelectionChanged;
+        }
 
-            //var allLayers = Map.GetPolygonLayers();
-            //foreach (layer in allLayers)
-                //Polygon_cbx.Items.Add(layer);
+        private void SelectionChanged(object sender, EventArgs e)
+        {
+            if (PolygonLayerList.SelectedValue != null)
+                getPolygons((IMapPolygonLayer)PolygonLayerList.SelectedValue);
+        }
 
-            //AppManager _mapArgs;
+        private void populatePolygonLayerDropdown()
+        {
+            var map = (Map)App.Map;
+            Dictionary<IMapPolygonLayer, string> layer = new Dictionary<IMapPolygonLayer, string>();
 
-            //var allLayers = ((Map)_mapArgs.Map).GetAllLayers();
-            //Polygon_cbx.Items.Add(allLayers);
-            //foreach (layer in allLayers)
-                //Polygon_cbx.Items.Add(layer);
+            foreach (var polygonLayer in map.GetAllLayers().OfType<IMapPolygonLayer>().Reverse())
+                layer.Add(polygonLayer, polygonLayer.LegendText);
 
-            
+            if (layer.Count > 0)
+            {
+                PolygonLayerList.DataSource = new BindingSource(layer, null);
+                PolygonLayerList.DisplayMember = "Value";
+                PolygonLayerList.ValueMember = "Key";
+            }
+        }
 
+        private void populateSites()
+        {
+            var map = (Map)App.Map;
+            Dictionary<IMapPointLayer, string> layer = new Dictionary<IMapPointLayer, string>();
+
+            foreach (var pointLayer in map.GetAllLayers().OfType<IMapPointLayer>().Reverse())
+                layer.Add(pointLayer, pointLayer.LegendText);
+
+            if (layer.Count > 0)
+            {
+                SiteList.DataSource = new BindingSource(layer, null);
+                SiteList.DisplayMember = "Value";
+                SiteList.ValueMember = "Key";
+            }
+        }
+
+        private void populateVariables()
+        {
+            foreach(var item in SiteList.Items)
+            {
+                IMapPointLayer pointLayer = ((KeyValuePair<IMapPointLayer, string>)item).Key;
+                var features = pointLayer.DataSet.Features;
+                //Dictionay of points
+
+                foreach(var point in features)
+                {
+                    //point.Intersects(
+                }
+            }
+        }
+
+        private void getPolygons(IMapPolygonLayer polyLayer)
+        {
+            polygons.Features.Clear();
+
+            if (polyLayer.IsVisible && polyLayer.Selection.Count > 0)
+            {
+                foreach (var f in polyLayer.Selection.ToFeatureList())
+                {
+                    polygons.Features.Add(f);
+                }
+
+                polygons.Projection = App.Map.Projection;
+            }
+
+            populateVariables();
         }
 
     }
