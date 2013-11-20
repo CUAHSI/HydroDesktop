d6645e4035c384f3675add4f93548cf2c430ed44
jacobean <jacobean@localhost>
2013-11-20 10:32:45 -0700

    Further work on Aggregation plugin

---
+++ Source/Aggregation_Plugin/SiteData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotSpatial.Data;
+
+namespace Aggregation_Plugin
+{
+    class SiteData
+    {
+        public IFeature site;
+        public String siteCode;
+        public String variableCode;
+        public String variableName;
+        public int siteID;
+        public int variableID;
+    }
+}
