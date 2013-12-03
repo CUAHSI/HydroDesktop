e2977972b9177a387ceb158f2c02e4531b15c5d2
mogikanin <mogikanin.tir@gmail.com>
2013-12-03 15:09:14 +0400

    Updated nuget config for Search3, also nuget updated to latest version. #8816

---
+++ Source/HydroDesktop.Data.ODMCVServiceClient/Infrastructure/SOAP/ODMCVSoapServiceClient.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using HydroDesktop.Interfaces.ObjectModel;
+
+namespace HydroDesktop.Data.ODMCVServiceClient.Infrastructure.SOAP
+{
+    class ODMCVSoapServiceClient : ServiceReference.ODMCV.ODMCVServiceSoapClient, IODMCVServiceClient
+    {
+        protected ODMCVSoapServiceClient()
+        {
+        }
+
+        public static ODMCVSoapServiceClient CreateNew()
+        {
+            return new ODMCVSoapServiceClient();
+        }
+
+        #region Implementation of IODMCVServiceClient
+
+        public new IEnumerable<Unit> GetUnits()
+        {
+            var units = base.GetUnits();
+            using (var textReader = new StringReader(units))
+            using (var xmlReader = new XmlTextReader(textReader))
+            {
+                while (xmlReader.Read())
+                {
+                    if (xmlReader.IsStartElement() &&
+                        string.Equals(xmlReader.Name, "Record", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var unit = new Unit();
+                        while (xmlReader.Read())
+                        {
+                            if (xmlReader.IsStartElement())
+                            {
+                                if (string.Equals(xmlReader.Name, "UnitsName", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    unit.Name = xmlReader.ReadString();
+                                }
+                                else if (string.Equals(xmlReader.Name, "UnitsType", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    unit.UnitsType = xmlReader.ReadString();
+                                }
+                                else if (string.Equals(xmlReader.Name, "UnitsAbbreviation",
+                                                       StringComparison.OrdinalIgnoreCase))
+                                {
+                                    unit.Abbreviation = xmlReader.ReadString();
+                                }
+                            }
+                            else
+                            {
+                                if (string.Equals(xmlReader.Name, "Record", StringComparison.OrdinalIgnoreCase))
+                                    break;
+                            }
+                        }
+                        yield return unit;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/HydroShare/Forms/logIn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HydroShare.Forms
+{
+    public partial class logIn : Form
+    {
+        public logIn()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            string username = this.userNameTB.Text;
+            string password = this.passwordTB.Text;
+
+            //compare user name and pw against database
+            this.Visible = false;
+            hydroshareLogIn log = new hydroshareLogIn(username, password);
+            log.StartPosition = FormStartPosition.CenterScreen;
+            log.Visible = false;
+
+            chooseUploadType uploadType = new chooseUploadType();
+            uploadType.StartPosition = FormStartPosition.CenterScreen;
+            uploadType.Visible = true;
+        }
+    }
+}
