bf32af0b0cadfa84efd98baaa3926d75aa787b0d
mogikanin <mogikanin.tir@gmail.com>
2013-12-07 18:00:46 +0400

    Suppress some compiler warnings

--- Source/Search3/WebServices/WebServicesReader.cs
+++ Source/Search3/WebServices/WebServicesReader.cs
@@ -11,7 +11,7 @@ namespace Search3.WebServices
         /// </summary>
         /// <param name="catalogSettings">Catalog settings</param>
         /// <returns>Collection of web services.</returns>
-        /// <exception cref="ArgumentNullException"><param name="catalogSettings"/> should be not null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="catalogSettings"/> should be not null.</exception>
         /// <exception cref="Exception">Any exception may raised during load process.</exception>
         public static IEnumerable<WebServiceNode> GetWebServices(CatalogSettings catalogSettings)
         {
