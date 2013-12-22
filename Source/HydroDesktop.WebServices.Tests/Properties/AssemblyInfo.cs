d540c2b1574732499bf703c18fba9135599bfb0d
mogikanin <mogikanin.tir@gmail.com>
2013-12-22 17:02:08 +0400

    WebServices.Tests: rename assembly name

--- Source/HydroDesktop.WebServices.Tests/Properties/AssemblyInfo.cs
+++ Source/HydroDesktop.WebServices.Tests/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebServiceTest")]
+[assembly: AssemblyTitle("HydroDesktop.WebServices.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebServiceTest")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyProduct("HydroDesktop.WebServices.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
