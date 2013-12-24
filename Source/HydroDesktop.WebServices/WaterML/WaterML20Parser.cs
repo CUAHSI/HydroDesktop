ac0b25a2c512100d6c68fab4c3efea0ac5a32875
mogikanin <mogikanin.tir@gmail.com>
2013-12-24 17:06:14 +0400

    parsing datavalues

--- Source/HydroDesktop.WebServices/WaterML/WaterML20Parser.cs
+++ Source/HydroDesktop.WebServices/WaterML/WaterML20Parser.cs
@@ -67,23 +67,36 @@ namespace HydroDesktop.WebServices.WaterML
                     var name = reader.Name.ToLower();
                     if (name == "wml2:observationmember")
                     {
+                        Series series = null;
+
                         var subReader = reader.ReadSubtree();
                         while (subReader.Read())
                         {
-                            if (subReader.NodeType == XmlNodeType.Element &&
-                                subReader.Name.ToLower() == "om:result")
+                            if (subReader.NodeType == XmlNodeType.Element)
                             {
-                                var sub2 = subReader.ReadSubtree();
-                                while (sub2.Read())
+                                switch (subReader.Name.ToLower())
                                 {
-                                    if (sub2.NodeType == XmlNodeType.Element &&
-                                        sub2.Name.ToLower() == "wml2:timeseries")
-                                        yield return ReadOneSeries(sub2.ReadSubtree());  
+                                    case "om:result":
+                                        var sub2 = subReader.ReadSubtree();
+                                        while (sub2.Read())
+                                        {
+                                            if (sub2.NodeType == XmlNodeType.Element)
+                                            {
+                                                switch (sub2.Name.ToLower())
+                                                {
+                                                    case "wml2:timeseries":
+                                                    case "wml2:measurementtimeseries":
+                                                        series = ReadOneSeries(sub2.ReadSubtree());
+                                                        break;
+                                                }
+                                            }
+                                        }
+                                        break;
                                 }
-                                subReader.Skip();
                             }
                         }
-                        reader.Skip();
+
+                        if (series != null) yield return series;
                     }
                 }
             }
@@ -106,12 +119,13 @@ namespace HydroDesktop.WebServices.WaterML
                                 switch (subReader.Name.ToLower())
                                 {
                                     case "wml2:timevaluepairmeasure":
-                                        series.DataValueList.Add(new DataValue());
+                                    case "wml2:measurementtvp":
+                                        var dv = ReadDataValue(subReader.ReadSubtree());
+                                        series.DataValueList.Add(dv);
+                                        break;
                                 }
                             }
                         }
-
-                        reader.Skip();
                     }
                 }
             }
@@ -127,212 +141,43 @@ namespace HydroDesktop.WebServices.WaterML
             return series;
         }
 
-        #region Old
-
-        public IList<Series> ParseGetValuesOld(Stream stream)
+        private static DataValue ReadDataValue(XmlReader reader)
         {
-            XmlDocument wmlDoc = new XmlDocument();
-            wmlDoc.Load(stream);
-
-            //get the data values and put them into a list of series
-            var seriesList = ReadDataValues(wmlDoc);
-
-            //site = readSite(wmlDoc);
-            //varInfo = readVariable(wmlDoc);
-
-            foreach (var series in seriesList)
+            double value = 0;
+            double utcOffset = 0;
+            var dateTime = DateTime.MinValue;
+            while (reader.Read())
             {
-                //ensure that properties are re-calculated
-                series.UpdateSeriesInfoFromDataValues();
-
-                //set the checked and creation date time
-                series.CreationDateTime = DateTime.Now;
-                series.LastCheckedDateTime = DateTime.Now;
-                series.UpdateDateTime = series.LastCheckedDateTime;
-            }
-            return seriesList;
-        }
-
-        private IList<Series> ReadDataValues(XmlDocument wmlDoc)
-        {
-            IList<Series> seriesList = new List<Series>();
-            XmlNodeList observations = wmlDoc.GetElementsByTagName("wml2:observationMember");
-
-            if (observations.Count == 0)
-                observations = wmlDoc.GetElementsByTagName("om:OM_Observation");
-
-            if (observations.Count == 0)
-            {
-                Series newSeries = new Series();
-                newSeries = ReadDataSeries(wmlDoc);
-
-                //add parsed series data to list
-                if (newSeries.GetValueCount() > 0)
-                    seriesList.Add(newSeries);
-            }
-
-            //loop through each series observation
-            foreach (XmlNode observation in observations)
-            {
-                Series newSeries = new Series();
-                XmlDocument xml = new XmlDocument();
-                xml.LoadXml(observation.OuterXml);
-                newSeries = ReadDataSeries(xml);
-
-                //add parsed series data to list
-                if (newSeries.GetValueCount() > 0)
-                    seriesList.Add(newSeries);
-            }
-
-            return seriesList;
-        }
-
-        private Series ReadDataSeries(XmlDocument xml)
-        {
-            Series newSeries = new Series();
-            XmlNodeList tvps = GetTVPList(xml);
-            GetSeriesMetadata(xml, newSeries);
-            GetDefaultNodeMetadata(xml, newSeries);
-
-            //add Begin and End datetime to the series
-            try
-            {
-                XmlNode begin = xml.GetElementsByTagName("gml:beginPosition").Item(0);
-                XmlNode end = xml.GetElementsByTagName("gml:endPosition").Item(0);
-
-                if (begin.InnerText.Length > 19)
-                    newSeries.BeginDateTime = Convert.ToDateTime(begin.InnerText.Remove(19));
-                else
-                    newSeries.BeginDateTime = Convert.ToDateTime(begin.InnerText);
-
-                if (end.InnerText.Length > 19)
-                    newSeries.EndDateTime = Convert.ToDateTime(end.InnerText.Remove(19));
-                else
-                    newSeries.EndDateTime = Convert.ToDateTime(end.InnerText);
-            }
-            catch { }
-
-            //add each Time-Value-Pair to the series
-            foreach (XmlNode tvp in tvps)
-            {
-                Double value = -9999;
-                DateTime time = new DateTime();
-                double utcOffset = 0;
-
-                //parse Time-Value-Pair
-                foreach (XmlNode child in tvp.ChildNodes)
+                if (reader.NodeType == XmlNodeType.Element)
                 {
-                    if (!String.IsNullOrEmpty(child.Name))
+                    switch (reader.Name.ToLower())
                     {
-                        if (child.Name.ToLower() == "wml2:value" && child.InnerText != "")
-                            value = Double.Parse(child.InnerText);
-                        if (child.Name.ToLower() == "wml2:time" && child.InnerText != "")
-                        {
-                            String dateTime = child.InnerText;
-                            String utcoffset = "Z";
-                            if (dateTime.Length > 19)
+                        case "wml2:value":
+                            reader.Read();
+                            value = Convert.ToDouble(reader.Value, CultureInfo.InvariantCulture);
+                            break;
+                        case "wml2:time":
+                            reader.Read();
+                            var dStr = reader.Value;
+                            // without utc offset : 2008-04-05T23:00:00
+                            // with utc offset    : 2005-08-05T22:30:00-07:00
+                            var withUtcOffset = dStr.Count(d => d == ':') == 3;
+
+                            const string baseFormat = "yyyy-MM-ddTHH:mm:ss";
+                            var dateStr = dStr.Substring(0, baseFormat.Length);
+                            dateTime = DateTime.ParseExact(dateStr, baseFormat, CultureInfo.InvariantCulture);
+                            if (withUtcOffset)
                             {
-                                utcoffset = dateTime.Substring(19);
-                                dateTime = dateTime.Remove(19);
+                                utcOffset = ParserHelper.ConvertUtcOffset(dStr.Substring(baseFormat.Length));
                             }
-                            time = Convert.ToDateTime(dateTime);
-                            if (utcoffset.ToLower() == "z")
-                                utcOffset = 0;
-                            else
-                                utcOffset = ConvertUtcOffset(utcoffset);
-                        }
-                    }
-                }
+                            
+                            break;
 
-                //add parsed Time-Value-Pair to series
-                DataValue dataValue = new DataValue(value, time, utcOffset);
-                newSeries.DataValueList.Add(dataValue);
-                dataValue.Series = newSeries;
-            }
-            return newSeries;
-        }
-
-        private XmlNodeList GetTVPList(XmlDocument xml)
-        {
-            XmlNodeList tvps = xml.GetElementsByTagName("wml2:MeasurementTVP");
-            if (tvps.Count == 0)
-                tvps = xml.GetElementsByTagName("wml2:CategoricalTVP");
-            if (tvps.Count == 0)
-                tvps = xml.GetElementsByTagName("wml2:TimeValuePair");
-
-            return tvps;
-        }
-
-        private void GetSeriesMetadata(XmlDocument xml, Series newSeries)
-        {
-            XmlNodeList meta = xml.GetElementsByTagName("wml2:MeasurementTimeseriesMetadata");
-            if (meta.Count == 0)
-                meta = xml.GetElementsByTagName("wml2:TimeseriesMetadata");
-            if (meta.Count == 0)
-                meta = xml.GetElementsByTagName("wml2:CategoricalTimeseriesMetadata");
-        }
-
-        private void GetDefaultNodeMetadata(XmlDocument xml, Series newSeries)
-        {
-            XmlNodeList meta = xml.GetElementsByTagName("wml2:DefaultTVPMeasurementMetadata");
-            if (meta.Count == 0)
-                meta = xml.GetElementsByTagName("wml2:DefaultTVPCategoricalMetadata");
-            if (meta.Count == 0)
-                meta = xml.GetElementsByTagName("wml2:DefaultTVPMetadata");
-
-            if (meta.Count != 0)
-            {
-                foreach (XmlNode child in meta.Item(0).ChildNodes)
-                {
-                    if (child.Name == "wml2:quality")
-                    {
-
-                    }
-                    if (child.Name == "wml2:qualifier")
-                    {
-
-                    }
-                    if (child.Name == "wml2:processing")
-                    {
-                    }
-                    if (child.Name == "wml2:uom")
-                    {
-
-                    }
-                    if (child.Name == "wml2:interpolationType")
-                    {
-                        foreach (XmlAttribute attribute in child.Attributes)
-                        {
-                            if (attribute.Name == "xlink:href")
-                            {
-                                string interpolationType = attribute.InnerText.Split('/').Last();
-                                if (newSeries.Variable == null)
-                                    newSeries.Variable = new Variable();
-                                newSeries.Variable.DataType = interpolationType;
-                            }
-                        }
                     }
                 }
             }
+
+            return new DataValue(value, dateTime, utcOffset);
         }
-
-
-        /// <summary>
-        /// Converts the 'UTC Offset' value to a double digit in hours
-        /// </summary>
-        private double ConvertUtcOffset(string offsetString)
-        {
-            int colonIndex = offsetString.IndexOf(":", StringComparison.Ordinal);
-            double minutes = 0.0;
-            double hours = 0.0;
-            if (colonIndex > 0 && colonIndex < offsetString.Length - 1)
-            {
-                minutes = Convert.ToDouble(offsetString.Substring(colonIndex + 1), CultureInfo.InvariantCulture);
-                hours = Convert.ToDouble((offsetString.Substring(0, colonIndex)), CultureInfo.InvariantCulture);
-            }
-            return hours + (minutes / 60.0);
-        }
-
-        #endregion
     }
 }
