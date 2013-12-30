c789378573a511ce7e85bd55aa42071e7032086b
ericch <ericch@localhost>
2013-12-30 11:15:41 -0700

    Additional work on welcome screen

--- Source/HydroDesktop.MainPlugin/Properties/Resources.Designer.cs
+++ Source/HydroDesktop.MainPlugin/Properties/Resources.Designer.cs
@@ -63,9 +63,29 @@ namespace HydroDesktop.Main.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap check_mark {
+            get {
+                object obj = ResourceManager.GetObject("check_mark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CuahsiLogo38 {
             get {
                 object obj = ResourceManager.GetObject("CuahsiLogo38", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download {
+            get {
+                object obj = ResourceManager.GetObject("download", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
