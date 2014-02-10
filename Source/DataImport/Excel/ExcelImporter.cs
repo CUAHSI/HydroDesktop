6f228bcf2e171863e0b837f1a2d9f5cc2835ac99
gespinoza <gespinoza@localhost>
2014-02-09 18:20:32 -0600

    After Tim comments

---
+++ Source/DataImport/Excel/ExcelImporter.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using DataImport.CommonPages;
+using DataImport.CommonPages.Complete;
+using DataImport.CommonPages.Progress;
+using DataImport.CommonPages.SelectLayer;
+using DataImport.DataTableImport;
+using Excel;
+using Wizard.UI;
+
+namespace DataImport.Excel
+{
+    class ExcelImporter : IWizardImporter
+    {
+        public string Filter
+        {
+            get { return "Excel File (*.xls;*.xlsx)|*.xls;*.xlsx"; }
+        }
+
+        public bool CanImportFromFile(string pathToFile)
+        {
+            var extension = Path.GetExtension(pathToFile);
+            return string.Equals(extension, ".xlsx", StringComparison.InvariantCultureIgnoreCase) ||
+                   string.Equals(extension, ".xls", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public IWizardImporterSettings GetDefaultSettings()
+        {
+            return new ExcelImportSettings();
+        }
+
+        public IImporter GetImporter()
+        {
+            return new DataTableImporterImpl();
+        }
+
+        public ICollection<WizardPage> GetWizardPages(WizardContext context)
+        {
+            return new Collection<WizardPage>
+                       {
+                           new FormatOptionsPage(context),
+                           new FieldPropertiesPage(context),
+                           new DataLayerPage(context),
+                           new ProgressPage(context),
+                           new CompletePage(),
+                       };
+        }
+
+        public void UpdatePreview(IWizardImporterSettings settings)
+        {
+            var excelSettings = (ExcelImportSettings)settings;
+            if (excelSettings.DataSet == null)
+            {
+                var ds = AsDataSet(settings);
+                excelSettings.DataSet = ds;
+                if (ds.Tables.Count > 0)
+                {
+                    excelSettings.SheetName = ds.Tables[0].TableName;
+                }
+            }
+
+            var result = excelSettings.DataSet.Tables.Contains(excelSettings.SheetName)
+                             ? excelSettings.DataSet.Tables[excelSettings.SheetName]
+                             : new DataTable();
+            settings.Preview = result;
+        }
+
+        public void UpdateData(IWizardImporterSettings settings)
+        {
+            settings.Data = settings.Preview;
+        }
+
+        #region Private methods
+
+        private DataSet AsDataSet(IImporterSettings settings)
+        {
+            var fileName = settings.PathToFile;
+
+            if (!File.Exists(fileName))
+            {
+                return new DataSet();
+            }
+
+            var extension = Path.GetExtension(fileName);
+            IExcelDataReader reader = null;
+            var stream = File.Open(fileName, FileMode.Open);
+            try
+            {
+                switch (extension)
+                {
+                    case ".xls":
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                        break;
+                    case ".xlsx":
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                        break;
+                    default:
+                        goto case ".xls";
+                }
+
+                reader.IsFirstRowAsColumnNames = true;
+                var ds = reader.AsDataSet();
+                return ds;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                stream.Close();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/HydroDesktop.MainApplication/NavigationControl.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DotSpatial.Controls;
+using DotSpatial.Topology;
+
+namespace HydroDesktop.MainApplication
+{
+    public partial class NavigationControl : UserControl
+    {
+        private AppManager appManager;
+        private Button button6;
+
+        public NavigationControl(AppManager appManager)
+        {
+            this.appManager = appManager;
+            button6 = new Button();
+            button6.Size = Size.Empty;
+            Controls.Add(button6);
+            InitializeComponent();
+
+            appManager.Map.FunctionModeChanged += Map_FunctionModeChanged;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                case Keys.Space:
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void Map_FunctionModeChanged(object sender, EventArgs e)
+        {
+            if (appManager.Map.FunctionMode == FunctionMode.Pan)
+                button1.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.ZoomIn)
+                button2.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.ZoomOut)
+                button3.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.Select)
+                button4.Focus();
+            else
+                button6.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            appManager.Map.FunctionMode = FunctionMode.Pan;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            appManager.Map.FunctionMode = FunctionMode.ZoomIn;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            appManager.Map.FunctionMode = FunctionMode.ZoomOut;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            appManager.Map.FunctionMode = FunctionMode.Select;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (appManager.Map.FunctionMode == FunctionMode.Pan)
+                button1.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.ZoomIn)
+                button2.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.ZoomOut)
+                button3.Focus();
+            else if (appManager.Map.FunctionMode == FunctionMode.Select)
+                button4.Focus();
+            else
+                button6.Focus();
+
+            IEnvelope env; 
+            appManager.Map.MapFrame.ClearSelection(out env);
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            var ToolTip1 = new ToolTip();
+            ToolTip1.SetToolTip(button1, "Pan");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            var ToolTip2 = new ToolTip();
+            ToolTip2.SetToolTip(button2, "Zoom In");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            var ToolTip3 = new ToolTip();
+            ToolTip3.SetToolTip(button3, "Zoom Out");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            var ToolTip4 = new ToolTip();
+            ToolTip4.SetToolTip(button4, "Select");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void button5_MouseHover(object sender, EventArgs e)
+        {
+            var ToolTip5 = new ToolTip();
+            ToolTip5.SetToolTip(button5, "Deselect");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void NavigationControl_MouseHover(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Arrow;
+        }
+    }
+}
