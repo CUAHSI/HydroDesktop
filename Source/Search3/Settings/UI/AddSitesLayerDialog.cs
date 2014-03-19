64e45116e4fb2c594f29aa69075fd1e02cbe5607
ericch <ericch@localhost>
2014-03-19 12:58:35 -0600

    List of variables can now be downloaded

--- Source/Search3/Settings/UI/AddSitesLayerDialog.cs
+++ Source/Search3/Settings/UI/AddSitesLayerDialog.cs
@@ -6,6 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using HydroDesktop.WebServices.WaterOneFlow;
+using HydroDesktop.Interfaces.ObjectModel;
+using System.Net;
+using System.IO;
 
 namespace Search3.Settings.UI
 {
@@ -15,5 +19,55 @@ namespace Search3.Settings.UI
         {
             InitializeComponent();
         }
+
+        WaterOneFlowClient waterOneFlowClient;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           waterOneFlowClient = new WaterOneFlowClient(urlConnectionTextbox.Text);
+    
+           // Update service info in the metadata database
+           var waterOneFlowServiceInfo = waterOneFlowClient.ServiceInfo;
+           // Get all sites for this service
+           IList<Site> siteList;
+           siteList = waterOneFlowClient.GetSites();
+           var variableList = new List<Tuple<string, string>>();
+           foreach (var site in siteList)
+           {
+               // Get series for this site
+               IList<SeriesMetadata> currentSeriesList;
+               try
+               {
+                   currentSeriesList = waterOneFlowClient.GetSiteInfo(site.Code);
+               }
+               catch (WebException ex)
+               {
+                   if (ex.Response != null)
+                   {
+                       var rdr = new StreamReader(ex.Response.GetResponseStream());
+                       rdr.ReadToEnd();
+                   }
+
+                   continue;
+               }
+               // Save series info to metadata cache database
+               foreach (var series in currentSeriesList)
+               {
+                   var variable = new Tuple<string, string>(series.Variable.Code, series.Variable.Name);
+                   variableList.Add(variable);
+               }
+           }
+           foreach (var variable in variableList)
+           {
+               this.variablesListBox.Items.Add(variable.Item2, false);
+               
+           }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+   
     }
 }
