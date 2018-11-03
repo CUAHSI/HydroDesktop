cf87b7cc1492eed6f38f3a4e4adf19ae424ab40c
DANAMES\Dan <dan.ames@byu.edu>
2018-11-02 18:51:46 -0600

    Updating the version number and copyright date
    Updating the version number and copyright date

--- Source/AssemblyInfoShared.cs
+++ Source/AssemblyInfoShared.cs
@@ -15,5 +15,7 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
+
+
--- Source/Core/HydroDesktop.MainApplication/Properties/AssemblyInfo.cs
+++ Source/Core/HydroDesktop.MainApplication/Properties/AssemblyInfo.cs
@@ -4,12 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("HydroDesktop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Client tool for the CUAHSI Hydrologic Information System built by Dan Ames @ Brigham Young University")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CUAHSI")]
 [assembly: AssemblyProduct("HydroDesktop")]
-[assembly: AssemblyCopyright("Copyright © CUAHSI 2012")]
+
+[assembly: AssemblyCopyright("© CUAHSI & Dan Ames 2012-Now")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,3 +22,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("621b23cd-d483-4d02-9f0e-5d4f201eea17")]
+
